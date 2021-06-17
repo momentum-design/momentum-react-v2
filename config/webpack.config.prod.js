6b65d1a28a4a024dd62ccfff58d0935fd3d6c220
Colin Read <coread@cisco.com>
2021-06-17 13:24:59 +0100

    fix(lint): unused import

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
