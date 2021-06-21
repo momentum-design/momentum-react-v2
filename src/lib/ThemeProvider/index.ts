c32ec57b15a518de6c34ee9b406c80353b188d13
Tim Scheuering <timsch@cisco.com>
2021-06-21 10:51:26 -0400

    feat(components/theme-provider): generate core component

---
+++ src/lib/ThemeProvider/index.ts
@@ -0,0 +1,6 @@
+import { default as ThemeProvider } from './ThemeProvider';
+import { ThemeProviderProps as Props } from './ThemeProvider.types';
+
+export type ThemeProviderProps = Props;
+
+export default ThemeProvider;
