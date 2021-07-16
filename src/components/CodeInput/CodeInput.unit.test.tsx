6d653de01f5acca972879add239f7eb45159b927
Colin Read <coread@cisco.com>
2021-07-16 13:37:19 +0100

    feat(codeinput): add test for entering code

--- src/components/CodeInput/CodeInput.unit.test.tsx
+++ src/components/CodeInput/CodeInput.unit.test.tsx
@@ -19,4 +19,16 @@ describe('CodeInput', () => {
       expect(component.find('.md-code-input-character').length).toBe(6);
     });
   });
+
+  describe('digit entry', () => {
+    it('fires codeComplete when number of digits reached', () => {
+      const spy = jest.fn();
+      const codeInput = mount(<CodeInput numDigits={3} onComplete={spy} />);
+      codeInput.simulate('click');
+      codeInput.find('input').hostNodes().simulate('change', { target: { value: '123' } });
+      const input = codeInput.find('input');
+      expect(input.props().value).toEqual('123');
+      expect(spy).toBeCalledWith('123');
+    });
+  });
 });
