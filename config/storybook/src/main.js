7538a9574071743dead73d2cc137f0f13ce07124
Colin Read <coread@cisco.com>
2021-07-21 10:47:16 +0100

    feat(codeinput): svgr and icon dependency

--- config/storybook/src/main.js
+++ config/storybook/src/main.js
@@ -13,6 +13,10 @@ module.exports = {
   },
   webpackFinal: (config) => {
     config.resolve.alias['@momentum-ui/react'] = '../../../src/index';
+    config.module.rules.unshift({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    })
 
     return config;
   },
