c18d4efc4d822faba697dbdad41b0db9c6c8b705
Catalin Torge <torgeadelin@gmail.com>
2021-08-03 19:17:50 +0100

    Revert "Revert "feat(codeinput): code input component""

---
+++ src/components/InputMessage/InputMessage.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from 'react';
+
+import './InputMessage.style.scss';
+import { Props } from './InputMessage.types';
+
+import Icon from '../Icon';
+
+const InputMessage = (props: Props): ReactElement => {
+  const { message, level = 'none' } = props;
+
+  return (
+    <div className="md-input-message-wrapper">
+      <div className="md-input-message" role="alert" message-level={level}>
+        <div className="md-input-message--icon">
+          {(level == 'error' || level == 'warning') && (
+            <Icon name="warning" weight="bold" scale={20}></Icon>
+          )}
+        </div>
+        <div className="md-input-message--text">{message}</div>
+      </div>
+    </div>
+  );
+};
+
+/**
+ * Message for use under Inputs that shows text with icon and color corresponding to level
+ */
+
+export default InputMessage;
