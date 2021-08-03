c804db120c3c857421d223f0c6c045663a3eb2da
Colin Read <coread@cisco.com>
2021-08-03 11:33:40 +0100

    feat(codeinput): remove theming from story

--- src/components/CodeInput/CodeInput.stories.tsx
+++ src/components/CodeInput/CodeInput.stories.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Story } from '@storybook/react';
 
-import ThemeProvider, { ThemeNames, THEME_PROVIDER_CONSTANTS } from '../ThemeProvider';
 import CodeInput, { CodeInputProps } from './';
 import { Message } from './CodeInput.types';
 
@@ -19,18 +18,14 @@ export default {
     numDigits: {
       description: 'Number of boxes',
     },
-    theme: {
-      description: 'The name of the target theme to use.',
-      options: [...Object.values(THEME_PROVIDER_CONSTANTS.THEME_NAMES)],
-      control: { type: 'select' },
-    },
     messageArr: {
-      description: 'The name of the target theme to use.',
+      description: 'The list of messages to be passed in',
       options: Object.keys(messageArrOptions),
       mapping: messageArrOptions,
       control: { type: 'select' },
     },
     disabled: {
+      description: 'Whether the input is disabled or not',
       control: { type: 'boolean' },
     },
     onCompleteChoice: {
@@ -42,51 +37,39 @@ export default {
 };
 
 interface StoryProps extends CodeInputProps {
-  theme: ThemeNames;
   messageArr: [];
   onCompleteChoice;
 }
 
 const Template: Story<StoryProps> = (args) => {
-  const { theme, onCompleteChoice } = args;
+  const { onCompleteChoice } = args;
   const [messageArrInt, setMessageArr] = useState<Message[]>(args.messageArr);
   const [isDisabled, setDisabled] = useState(args.disabled);
 
   return (
-    <ThemeProvider theme={theme}>
-      <div
-        style={{
-          display: 'inline-block',
-          padding: '2rem',
-          backgroundColor: 'var(--theme-background-solid-primary-normal)',
-        }}
-      >
-        <CodeInput
-          numDigits={args.numDigits}
-          messageArr={messageArrInt}
-          disabled={isDisabled}
-          onComplete={(code) => {
-            switch (onCompleteChoice) {
-              case 'show error':
-                setMessageArr([{ message: 'test', type: 'error' }]);
-                break;
-              case 'go disabled':
-                setDisabled(true);
-                break;
-              case 'alert':
-                alert(`code is ${code}`);
-            }
-          }}
-        />
-      </div>
-    </ThemeProvider>
+    <CodeInput
+      numDigits={args.numDigits}
+      messageArr={messageArrInt}
+      disabled={isDisabled}
+      onComplete={(code) => {
+        switch (onCompleteChoice) {
+          case 'show error':
+            setMessageArr([{ message: 'test', type: 'error' }]);
+            break;
+          case 'go disabled':
+            setDisabled(true);
+            break;
+          case 'alert':
+            alert(`code is ${code}`);
+        }
+      }}
+    />
   );
 };
 
 const Story1 = Template.bind({});
 
 Story1.args = {
-  theme: 'lightWebex',
   numDigits: 6,
   messageArr: 'empty',
   disabled: false,
