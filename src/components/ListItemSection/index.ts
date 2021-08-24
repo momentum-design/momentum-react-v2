c9a244136c4a6c9cb2b4aab5fd84bdb30113b735
Catalin <ctorge@cisco.com>
2021-08-24 14:07:14 +0100

    feat(menu-list-box): add menu list box component

--- src/components/ListItemSection/index.ts
+++
@@ -1,9 +0,0 @@
-import { default as ListItemSection } from './ListItemSection';
-import { Props } from './ListItemSection.types';
-import * as CONSTANTS from './ListItemSection.constants';
-
-export { CONSTANTS as LIST_ITEM_SECTION_CONSTANTS };
-
-export type ListItemSectionProps = Props;
-
-export default ListItemSection;
