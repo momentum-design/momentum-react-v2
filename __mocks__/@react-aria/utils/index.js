3c6e7a680a3376b8a3a921e9a4aa06ae0509ce31
Catalin <ctorge@cisco.com>
2021-08-24 12:26:13 +0100

    feat(flex): add doc

--- __mocks__/@react-aria/utils/index.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const utils = require('@react-aria/utils/dist/main');
-
-module.exports = {
-  ...utils,
-  useId: () => 'test-ID',
-};
