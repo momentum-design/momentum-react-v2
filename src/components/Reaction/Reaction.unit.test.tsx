18c05eb3552e1110fbeffbb6b0c2194cb1548aac
Rob Stachofsky <rstachof@cisco.com>
2021-08-31 16:21:28 -0400

    feat(reactions): implement SVG reactions

---
+++ src/components/Reaction/Reaction.unit.test.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Reaction, { REACTION_CONSTANTS as CONSTANTS } from './';
+
+describe('<Reaction />', () => {
+  describe('snapshot', () => {
+    it('should match snapshot', () => {
+      expect.assertions(1);
+
+      const container = mount(<Reaction />);
+
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should match snapshot with className', () => {
+      expect.assertions(1);
+
+      const className = 'example-class';
+
+      const container = mount(<Reaction className={className} />);
+
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should match snapshot with id', () => {
+      expect.assertions(1);
+
+      const id = 'example-id';
+
+      const container = mount(<Reaction id={id} />);
+
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should match snapshot with style', () => {
+      expect.assertions(1);
+
+      const style = { color: 'pink' };
+
+      const container = mount(<Reaction style={style} />);
+
+      expect(container).toMatchSnapshot();
+    });
+
+    /* ...additional snapshot tests... */
+  });
+
+  describe('attributes', () => {
+    it('should have its wrapper class', () => {
+      expect.assertions(1);
+
+      const element = mount(<Reaction />)
+        .find(Reaction)
+        .getDOMNode();
+
+      expect(element.classList.contains(CONSTANTS.STYLE.wrapper)).toBe(true);
+    });
+
+    it('should have provided class when className is provided', () => {
+      expect.assertions(1);
+
+      const className = 'example-class';
+
+      const element = mount(<Reaction className={className} />)
+        .find(Reaction)
+        .getDOMNode();
+
+      expect(element.classList.contains(className)).toBe(true);
+    });
+
+    it('should have provided id when id is provided', () => {
+      expect.assertions(1);
+
+      const id = 'example-id';
+
+      const element = mount(<Reaction id={id} />)
+        .find(Reaction)
+        .getDOMNode();
+
+      expect(element.id).toBe(id);
+    });
+
+    it('should have provided style when style is provided', () => {
+      expect.assertions(1);
+
+      const style = { color: 'pink' };
+      const styleString = 'color: pink;';
+
+      const element = mount(<Reaction style={style} />)
+        .find(Reaction)
+        .getDOMNode();
+
+      expect(element.getAttribute('style')).toBe(styleString);
+    });
+
+    /* ...additional attribute tests... */
+  });
+
+  describe('actions', () => {
+    /* ...action tests... */
+  });
+});
---
+++ src/hooks/useDynamicJSONImport.ts
@@ -0,0 +1,51 @@
+import { useRef, useState, useEffect } from 'react';
+
+interface UseDynamicJSONImportOptions {
+  onCompleted?: (name: string, animationData: any) => void;
+  onError?: (err: Error) => void;
+}
+
+interface UseDynamicJSONImportReturn {
+  error?: Error;
+  loading?: boolean;
+  animationData?: any;
+}
+
+/**
+ * Dynamically load JSON animation data into a Ref object which
+ * can be rendered to the dom as a react component.
+ * @param name The name of the icon
+ * @param options onCompleted, onError callbacks
+ * @returns error, loading, SvgIcon
+ */
+function useDynamicJSONImport(
+  name: string,
+  options: UseDynamicJSONImportOptions = {}
+): UseDynamicJSONImportReturn {
+  const ImportedJSONRef = useRef<any | any>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error>(undefined);
+
+  const { onCompleted, onError } = options;
+  useEffect(() => {
+    setLoading(true);
+    const importJSON = async (): Promise<void> => {
+      try {
+        setError(undefined);
+        ImportedJSONRef.current =
+          await require(`@momentum-ui/animations/lottie/reactions/${name}.json`);
+        onCompleted?.(name, ImportedJSONRef.current);
+      } catch (err) {
+        onError?.(err);
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    importJSON();
+  }, [name, onCompleted, onError]);
+
+  return { error, loading, animationData: ImportedJSONRef.current };
+}
+
+export { useDynamicJSONImport };
