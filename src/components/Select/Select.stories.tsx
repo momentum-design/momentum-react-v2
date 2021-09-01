fab9071183167878cb647a11c642bcce13cf46fa
Catalin <ctorge@cisco.com>
2021-09-01 17:55:23 +0100

    feat(select): refactor stories

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,33 +1,20 @@
+/* eslint-disable react/display-name */
 /* eslint-disable @typescript-eslint/ban-types */
-import React, { FC, ReactElement } from 'react';
-import { Story } from '@storybook/react';
+import React, { ReactElement } from 'react';
 import { Item, Section } from '@react-stately/collections';
 import { action } from '@storybook/addon-actions';
-import Select, { SelectProps } from '.';
-import {
-  Title,
-  Subtitle,
-  Description,
-  Primary,
-  ArgsTable,
-  PRIMARY_STORY,
-} from '@storybook/addon-docs';
+import Select from '.';
+
+import { DocumentationPage } from '../../storybook/helper.stories.docs';
 
 import Documentation from './Select.documentation.mdx';
 import Icon from '../Icon';
 import Flex from '../Flex';
+
+import StyleDocs from '../../storybook/docs.stories.style.mdx';
 import argTypes from './Select.stories.args';
 
-const DocsPage: FC = () => (
-  <>
-    <Title />
-    <Subtitle />
-    <Description />
-    <Documentation />
-    <Primary />
-    <ArgsTable story={PRIMARY_STORY} />
-  </>
-);
+import { MultiTemplate, Template } from '../../storybook/helper.stories.templates';
 
 export default {
   title: 'Momentum UI/Select',
@@ -35,11 +22,8 @@ export default {
   parameters: {
     expanded: true,
     docs: {
-      page: DocsPage,
+      page: DocumentationPage(Documentation, StyleDocs),
     },
-  },
-  argTypes: {
-    ...argTypes,
   },
 };
 
@@ -50,86 +34,6 @@ type SelectData = {
   children?: SelectData[];
 };
 
-const MultiTemplate =
-  (): Story<SelectProps<SelectData>> =>
-  // eslint-disable-next-line react/display-name
-  (args: SelectProps<SelectData>, { parameters }) => {
-    const { variants } = parameters;
-
-    const elements = variants.map((variant, index: number) => {
-      if (variant.items[0].children) {
-        return (
-          <div key={index}>
-            <Select<SelectData> {...args} {...variant}>
-              {(item) => (
-                <Section key={item.id} items={item.children} title={item.value}>
-                  {(item) => (
-                    <Item textValue={item.textValue} key={item.id}>
-                      {item.value}
-                    </Item>
-                  )}
-                </Section>
-              )}
-            </Select>
-          </div>
-        );
-      } else {
-        return (
-          <div key={index}>
-            <Select<SelectData> {...args} {...variant}>
-              {(item) => (
-                <Item textValue={item.textValue} key={item.id}>
-                  {item.value}
-                </Item>
-              )}
-            </Select>
-          </div>
-        );
-      }
-    });
-
-    return (
-      <div
-        style={{
-          height: '50vh',
-          display: 'grid',
-          gridTemplateColumns: `repeat(3, auto)`,
-          gap: '1.5rem',
-          alignItems: 'start',
-        }}
-      >
-        {elements}
-      </div>
-    );
-  };
-
-const SingleTemplate: Story<SelectProps<object>> = (args) => (
-  <div style={{ height: '15rem' }}>
-    <Select {...args}>
-      <Item>Red</Item>
-      <Item>Blue</Item>
-      <Item>Green</Item>
-      <Item>Yellow</Item>
-    </Select>
-  </div>
-);
-
-const SectionsTemplate: Story<SelectProps<object>> = (args) => (
-  <div style={{ height: '24rem' }}>
-    <Select {...args}>
-      <Section title="Colors">
-        <Item>Red</Item>
-        <Item>Blue</Item>
-        <Item>Yellow</Item>
-      </Section>
-      <Section title="Animals">
-        <Item>Dog</Item>
-        <Item>Cat</Item>
-      </Section>
-    </Select>
-  </div>
-);
-
 const singleItems: SelectData[] = [
   { id: 0, value: 'Red' },
   { id: 1, value: 'Blue' },
@@ -137,42 +41,105 @@ const singleItems: SelectData[] = [
   { id: 3, value: 'Yellow' },
 ];
 
-const Example = SingleTemplate.bind({});
+const Example = Template(Select).bind({});
 
 Example.args = {
   label: 'Single Value',
   placeholder: 'Select an option',
   onSelectionChange: action('onSelectionChange'),
+  children: [
+    <Item key="1">Red</Item>,
+    <Item key="2">Blue</Item>,
+    <Item key="3">Green</Item>,
+    <Item key="4">Yellow</Item>,
+  ],
 };
 
-const Sections = SectionsTemplate.bind({});
+Example.argTypes = { ...argTypes };
+
+const Sections = Template(Select).bind({});
 
 Sections.args = {
   label: 'With Sections',
   placeholder: 'Select an option',
   onSelectionChange: action('onSelectionChange'),
+  children: [
+    <Section key="0" title="Colors">
+      <Item>Red</Item>
+      <Item>Blue</Item>
+      <Item>Yellow</Item>
+    </Section>,
+    <Section key="1" title="Animals">
+      <Item>Dog</Item>
+      <Item>Cat</Item>
+    </Section>,
+  ],
 };
 
-const DisabledOptions = SectionsTemplate.bind({});
+Sections.argTypes = { ...argTypes };
+
+delete Sections.argTypes.label;
+delete Sections.argTypes.placeholder;
+delete Sections.argTypes.onSelectionChange;
+
+const DisabledOptions = Template(Select).bind({});
 
 DisabledOptions.args = {
   label: 'With Disabled Options',
   placeholder: 'Select an option',
   onSelectionChange: action('onSelectionChange'),
   disabledKeys: ['$.0.0', '$.0.1', '$.1.1'],
+  children: [
+    // If key is not provided the elements get generated ones
+    // eslint-disable-next-line react/jsx-key
+    <Section title="Colors">
+      <Item>Red</Item>
+      <Item>Blue</Item>
+      <Item>Yellow</Item>
+    </Section>,
+    // eslint-disable-next-line react/jsx-key
+    <Section title="Animals">
+      <Item>Dog</Item>
+      <Item>Cat</Item>
+    </Section>,
+  ],
 };
 
-const Common = MultiTemplate().bind({});
+DisabledOptions.argTypes = { ...argTypes };
+
+delete DisabledOptions.argTypes.label;
+delete DisabledOptions.argTypes.placeholder;
+delete DisabledOptions.argTypes.onSelectionChange;
+delete DisabledOptions.argTypes.disabledKeys;
+
+const Common = MultiTemplate(Select).bind({});
+
+Common.argTypes = { ...argTypes };
 
 Common.args = {
   placeholder: 'Select an option',
   onSelectionChange: action('onSelectionChange'),
+  style: { margin: '1rem' },
 };
+
+delete Common.argTypes.label;
+delete Common.argTypes.placeholder;
+delete Common.argTypes.onSelectionChange;
+delete Common.argTypes.disabledKeys;
 
 Common.parameters = {
   variants: [
-    { label: 'Default', items: singleItems },
-    { label: 'Disabled', items: singleItems, isDisabled: true },
+    {
+      label: 'Default',
+      items: singleItems,
+      children: (item) => <Item key={item.id}>{item.value}</Item>,
+    },
+    {
+      label: 'Disabled',
+      items: singleItems,
+      isDisabled: true,
+      children: (item) => <Item key={item.id}>{item.value}</Item>,
+    },
     {
       label: 'With Icon',
       items: [
@@ -207,6 +174,7 @@ Common.parameters = {
           ),
         },
       ],
+      children: (item) => <Item key={item.id}>{item.value}</Item>,
     },
     {
       label: 'With Sections & Label Section',
@@ -264,6 +232,15 @@ Common.parameters = {
           ],
         },
       ] as SelectData[],
+      children: (item: SelectData) => (
+        <Section key={item.id} items={item.children} title={item.value}>
+          {(item) => (
+            <Item textValue={item.textValue} key={item.id}>
+              {item.value}
+            </Item>
+          )}
+        </Section>
+      ),
     },
     {
       label: 'With Sections & No Label Section',
@@ -319,8 +296,22 @@ Common.parameters = {
           ],
         },
       ] as SelectData[],
+      children: (item: SelectData) => (
+        <Section key={item.id} items={item.children} title={item.value}>
+          {(item) => (
+            <Item textValue={item.textValue} key={item.id}>
+              {item.value}
+            </Item>
+          )}
+        </Section>
+      ),
     },
-    { label: 'Direction Top', items: singleItems, direction: 'top' },
+    {
+      label: 'Direction Top',
+      items: singleItems,
+      direction: 'top',
+      children: (item) => <Item key={item.id}>{item.value}</Item>,
+    },
   ],
 };
 
