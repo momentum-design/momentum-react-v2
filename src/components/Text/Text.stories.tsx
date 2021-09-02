56fc6bd706e8e0f218161c9ea5c24313698390e9
ndelmar <nataliadelmar2@gmail.com>
2021-09-02 15:00:49 +0100

    feat(components/text): typescript fixes

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { MultiTemplate, Template } from '../../storybook/helper.stories.templates';
 import { DocumentationPage } from '../../storybook/helper.stories.docs';
 import StyleDocs from '../../storybook/docs.stories.style.mdx';
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -9,32 +9,34 @@ const Text: FC<Props> = (props: Props) => {
   const { children, type = DEFAULTS.TYPE, className, id, style } = props;
 
   const getElement = () => {
-    switch(type){
+    switch (type) {
       case TYPES.DISPLAY:
       case TYPES.BANNER_TERTIARY:
       case TYPES.BANNER_SECONDARY:
       case TYPES.BANNER_PRIMARY:
-        return (<h1
-          className={classnames(STYLE.wrapper, STYLE.color, className)}
-          data-type={type}
-          id={id}
-          style={style}
-        >
-          {children}
-        </h1>)
+        return (
+          <h1
+            className={classnames(STYLE.wrapper, STYLE.color, className)}
+            data-type={type}
+            id={id}
+            style={style}
+          >
+            {children}
+          </h1>
+        );
         break;
-      
+
       case TYPES.TITLE:
-        return(
+        return (
           <h2
-          className={classnames(STYLE.wrapper, STYLE.color, className)}
-          data-type={type}
-          id={id}
-          style={style}
-        >
-          {children}
-        </h2>
-        )
+            className={classnames(STYLE.wrapper, STYLE.color, className)}
+            data-type={type}
+            id={id}
+            style={style}
+          >
+            {children}
+          </h2>
+        );
         break;
 
       case TYPES.HEADER_PRIMARY:
@@ -43,7 +45,7 @@ const Text: FC<Props> = (props: Props) => {
       case TYPES.HEADER_SECONDARY:
       case TYPES.HIGHLIGHT_SECONDARY:
       case TYPES.SUBHEADER_SECONDARY:
-        return(
+        return (
           <h3
             className={classnames(STYLE.wrapper, STYLE.color, className)}
             data-type={type}
@@ -52,12 +54,12 @@ const Text: FC<Props> = (props: Props) => {
           >
             {children}
           </h3>
-        )
+        );
         break;
 
       case TYPES.BODY_PRIMARY:
-      case TYPES.HIGHLIGHT_PRIMARY:
-        return(
+      case TYPES.HYPERLINK_PRIMARY:
+        return (
           <p
             className={classnames(STYLE.wrapper, STYLE.color, className)}
             data-type={type}
@@ -66,43 +68,41 @@ const Text: FC<Props> = (props: Props) => {
           >
             {children}
           </p>
-        )
+        );
         break;
-      
+
       case TYPES.BODY_SECONDARY:
       case TYPES.HYPERLINK_SECONDARY:
       case TYPES.HIGHLIGHT_COMPACT:
       case TYPES.BODY_COMPACT:
       case TYPES.LABEL_COMPACT:
-          return(
-            <small
-              className={classnames(STYLE.wrapper, STYLE.color, className)}
-              data-type={type}
-              id={id}
-              style={style}
-            >
-              {children}
-            </small>
-          )
-          break;
-      
-      default:
-        return(
-        <p
-          className={classnames(STYLE.wrapper, STYLE.color, className)}
-          data-type={type}
-          id={id}
-          style={style}
-        >
-          {children}
-        </p>
-      )
-    }
-  }
+        return (
+          <small
+            className={classnames(STYLE.wrapper, STYLE.color, className)}
+            data-type={type}
+            id={id}
+            style={style}
+          >
+            {children}
+          </small>
+        );
+        break;
 
-  return (
-    getElement()
-  );
+      default:
+        return (
+          <p
+            className={classnames(STYLE.wrapper, STYLE.color, className)}
+            data-type={type}
+            id={id}
+            style={style}
+          >
+            {children}
+          </p>
+        );
+    }
+  };
+
+  return getElement();
 };
 
 export default Text;
