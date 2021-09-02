c7c6be981644d5accbd11d00440883fba4f2b7db
Rob Stachofsky <rstachof@cisco.com>
2021-09-02 13:48:43 -0400

    feat(reactions): more test updates

--- src/components/Reaction/Reaction.stories.args.ts
+++ src/components/Reaction/Reaction.stories.args.ts
@@ -1,19 +1,55 @@
 import { commonStyles } from '../../storybook/helper.stories.argtypes';
 
+import { REACTION_CONSTANTS as CONSTANTS } from './';
+
 export default {
   ...commonStyles,
-  /**
-   * Below is an example. See [Storybook argTypes documentation]{@link https://storybook.js.org/docs/react/api/argtypes}.
-   */
-  children: {
-    description: 'Provides the child nodes for this element.', // Description of this prop.
-    control: { type: 'text' }, // Control
+  autoPlay: {
+    description: 'Boolean to autoplay the reaction emoji or not',
+    control: { type: 'boolean' },
     table: {
       type: {
-        summary: 'ReactNode', // Explicit type of this prop.
+        summary: 'boolean',
       },
       defaultValue: {
-        summary: 'undefined', // Default value when rendering this component.
+        summary: true,
+      },
+    },
+  },
+  loop: {
+    description: 'Number of times to play the animation.',
+    control: { type: 'number' },
+    table: {
+      type: {
+        summary: 'number',
+      },
+      defaultValue: {
+        summary: 1,
+      },
+    },
+  },
+  name: {
+    description: 'Name of the emoji animation.',
+    control: { type: 'text' },
+    table: {
+      type: {
+        summary: 'string',
+      },
+      defaultValue: {
+        summary: '',
+      },
+    },
+  },
+  size: {
+    description: 'Modifies the size of this `<Reaction />`.',
+    options: [undefined, ...Object.values(CONSTANTS.SIZES)],
+    control: { type: 'select' },
+    table: {
+      type: {
+        summary: 'number',
+      },
+      defaultValue: {
+        summary: CONSTANTS.DEFAULTS.SIZE,
       },
     },
   },
