7fdeef3e031a40fd53b7bbfedc8017b6ce748919
Colin Read <coread@cisco.com>
2021-09-20 13:30:23 +0100

    feat(input): constants

---
+++ src/components/TextInput/TextInput.constants.ts
@@ -0,0 +1,11 @@
+const CLASS_PREFIX = 'md-text-input';
+
+const STYLE = {
+  wrapper: `${CLASS_PREFIX}-wrapper`,
+  container: `${CLASS_PREFIX}-container`,
+  help: `${CLASS_PREFIX}-help`,
+  error: `${CLASS_PREFIX}-error`,
+  messages: `${CLASS_PREFIX}-messages`,
+};
+
+export { STYLE };
