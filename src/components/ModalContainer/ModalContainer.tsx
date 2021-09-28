6f4db7730500115d04a40944a9d268cdabac9899
Timothy Scheuering <timsch@cisco.com>
2021-09-28 11:36:46 -0400

    feat(components/modal-container): add arrow

--- src/components/ModalContainer/ModalContainer.tsx
+++ src/components/ModalContainer/ModalContainer.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
 import classnames from 'classnames';
 
-import { DEFAULTS, STYLE } from './ModalContainer.constants';
+import ModalArrow from '../ModalArrow';
+
+import { ARROWS, DEFAULTS, STYLE } from './ModalContainer.constants';
 import { Props } from './ModalContainer.types';
 import './ModalContainer.style.scss';
 
@@ -9,19 +11,24 @@ import './ModalContainer.style.scss';
  * The ModalContainer component.
  */
 const ModalContainer: FC<Props> = (props: Props) => {
-  const { className, children, color, elevation, id, isPadded, isRounded, style } = props;
+  const { arrow, className, children, color, elevation, id, isPadded, round, style } = props;
+
+  const arrowComponent = arrow ? <ModalArrow color={color} side={arrow} /> : undefined;
 
   return (
-    <div
-      className={classnames(className, STYLE.wrapper)}
-      data-color={color || DEFAULTS.COLOR}
-      data-elevation={elevation || DEFAULTS.ELEVATION}
-      data-padded={isPadded}
-      data-rounded={isRounded}
-      id={id}
-      style={style}
-    >
-      {children}
+    <div className={classnames(className, STYLE.wrapper)} id={id} style={style} data-arrow={arrow}>
+      {arrowComponent}
+      <div
+        className={STYLE.content}
+        data-color={color || DEFAULTS.COLOR}
+        data-elevation={elevation || DEFAULTS.ELEVATION}
+        data-horizontal-arrow={arrow === ARROWS.LEFT || arrow === ARROWS.RIGHT}
+        data-vertical-arrow={arrow === ARROWS.BOTTOM || arrow === ARROWS.TOP}
+        data-padded={isPadded}
+        data-round={round || DEFAULTS.ROUND}
+      >
+        {children}
+      </div>
     </div>
   );
 };
