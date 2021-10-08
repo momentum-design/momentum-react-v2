793014009a20bf691bcaf59e4f7ce1e10d8b11c8
Colin Read <coread@cisco.com>
2021-10-08 13:54:04 +0100

    feat(legacystories): fix build

--- config/storybook/generateMain.js
+++ config/storybook/generateMain.js
@@ -20,6 +20,7 @@ const generateMain = (dir) => {
     webpackFinal: (config) => {
       config.resolve.alias['@momentum-ui/react'] = path.resolve(REPO_ROOT, dir, 'index');
       config.resolve.alias['@momentum-ui/core/images'] = path.resolve(REPO_ROOT, 'images');
+      config.resolve.alias['legacystyles'] = path.resolve(REPO_ROOT, 'scss');
 
       // Add SVGR Loader
       // ========================================================
