58c6f2bae452f3fe93e016a50f6280390769bdc1
Colin Read <coread@cisco.com>
2021-10-08 12:31:40 +0100

    feat(legacystories): legacy stories

---
+++ src/legacy/Select/examples/KitchenSink.stories.tsx
@@ -0,0 +1,12 @@
+import LegacyKitchenSink from './KitchenSink';
+import React from 'react';
+
+export default {
+  title: 'Legacy/Select',
+  component: LegacyKitchenSink,
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const Legacy = () => <LegacyKitchenSink />;
+
+export { Legacy };
