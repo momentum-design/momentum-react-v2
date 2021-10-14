5c272ae26931fada4b23511fd7d0b18952a9a1c2
Timothy Scheuering <timsch@cisco.com>
2021-10-14 15:10:15 -0400

    fix(components/text): constant typing

--- src/components/Text/Text.constants.ts
+++ src/components/Text/Text.constants.ts
@@ -1,10 +1,12 @@
+import { FontStyle } from './Text.types';
+
 const CLASS_PREFIX = 'md-text';
 
 const DEFAULTS = {
   TYPE: 'body-primary',
 };
 
-const TYPES = {
+const TYPES: Record<string, FontStyle> = {
   DISPLAY: 'display',
   BANNER_TERTIARY: 'banner-tertiary',
   BANNER_PRIMARY: 'banner-primary',
