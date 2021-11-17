28eaaae156ec1ff74dae17af7c944fea989ba30d
Timothy Scheuering <timsch@cisco.com>
2021-11-17 16:30:46 -0500

    feat(storybook/arg-types): add hover and improve button

--- src/storybook/helper.stories.argtypes.ts
+++ src/storybook/helper.stories.argtypes.ts
@@ -135,6 +135,61 @@ const commonAriaFocusScope = {
   },
 };
 
+const commonAriaHover = {
+  isDisabled: {
+    control: { type: 'boolean' },
+    description: 'Whether this components events should be disabled.',
+    table: {
+      category: 'React Aria - Hover',
+      type: {
+        summary: 'boolean',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  onHoverStart: {
+    control: { type: 'none' },
+    description: 'Handler that is called when the hover interaction starts',
+    table: {
+      category: 'React Aria - Hover',
+      type: {
+        summary: '(e: HoverEvent) => void',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  onHoverEnd: {
+    control: { type: 'none' },
+    description: 'Handler that is called when the hover interaction ends',
+    table: {
+      category: 'React Aria - Hover',
+      type: {
+        summary: '(e: HoverEvent) => void',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  onHoverChange: {
+    control: { type: 'none' },
+    description: 'Handler that is called when the hover state changes',
+    table: {
+      category: 'React Aria - Hover',
+      type: {
+        summary: '(isHovering: boolean) => void',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+};
+
 const commonAriaOverlay = {
   isOpen: {
     control: { type: 'boolean' },
@@ -233,10 +288,89 @@ const commonAriaModal = {
 };
 
 const commonAriaButton = {
+  isDisabled: {
+    control: { type: 'boolean' },
+    description: 'Whether the button is disabled',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'boolean',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  children: {
+    control: { type: 'text' },
+    description: 'The content to display in the button',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'ReactNode',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
   onPress: {
     action: 'onPress',
+    description: 'Handler that is called when the press is released over the target.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: '(e: PressEvent) => void',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  onPressStart: {
+    action: 'onPressStart',
+    description: 'Handler that is called when a press interaction starts.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: '(e: PressEvent) => void',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  onPressEnd: {
+    action: 'onPressEnd',
     description:
-      'From [AriaButtonProps](https://react-spectrum.adobe.com/react-aria/useButton.html). Handler that is called when the press is released over the target.',
+      'Handler that is called when a press interaction ends, either over the target or when the pointer leaves the target.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: '(e: PressEvent) => void',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  onPressChange: {
+    action: 'onPressChange',
+    description: 'Handler that is called when the press state changes.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: '(isPressed: boolean) => void',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  onPressUp: {
+    action: 'onPressUp',
+    description:
+      'Handler that is called when a press is released over the target, regardless of whether it started on the target or not.',
     table: {
       category: 'React Aria - Button',
       type: {
@@ -248,9 +382,8 @@ const commonAriaButton = {
     },
   },
   autoFocus: {
-    action: 'autoFocus',
-    description:
-      'From [AriaButtonProps](https://react-spectrum.adobe.com/react-aria/useButton.html). Whether the element should receive focus on render.',
+    control: { type: 'boolean' },
+    description: 'Whether the element should receive focus on render.',
     table: {
       category: 'React Aria - Button',
       type: {
@@ -261,10 +394,48 @@ const commonAriaButton = {
       },
     },
   },
+  onFocus: {
+    action: 'onFocus',
+    description: 'Handler that is called when the element receives focus.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: '(e: FocusEvent) => void',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  onBlur: {
+    action: 'onBlur',
+    description: 'Handler that is called when the element loses focus.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: '(e: FocusEvent) => void',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  onFocusChange: {
+    action: 'onFocusChange',
+    description: "Handler that is called when the element's focus status changes.",
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: '(isFocused: boolean) => void',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
   onKeyDown: {
     action: 'onKeyDown',
-    description:
-      'From [AriaButtonProps](https://react-spectrum.adobe.com/react-aria/useButton.html). Handler that is called when a key is pressed.',
+    description: 'Handler that is called when a key is pressed.',
     table: {
       category: 'React Aria - Button',
       type: {
@@ -275,10 +446,146 @@ const commonAriaButton = {
       },
     },
   },
-  'aria-details': {
+  onKeyUp: {
+    action: 'onKeyUp',
+    description: 'Handler that is called when a key is released.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: '(e: KeyboardEvent) => void',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  href: {
+    control: { type: 'text' },
+    description: 'A URL to link to if elementType="a".',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'string',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  rel: {
     control: { type: 'text' },
     description:
-      'From [AriaButtonProps](https://react-spectrum.adobe.com/react-aria/useButton.html). Sets the `aria-details` value.',
+      'The relationship between the linked resource and the current page. See [MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel).',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'string',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  elementType: {
+    control: { type: 'none' },
+    description:
+      "The HTML element or React element used to render the button, e.g. 'div', 'a', or RouterLink.",
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'T | JSXElementConstructor<any>',
+      },
+      defaultValue: {
+        summary: 'button',
+      },
+    },
+  },
+  'aria-expanded': {
+    control: { type: 'boolean' },
+    description:
+      'Indicates whether the element, or another grouping element it controls, is currently expanded or collapsed.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'boolean',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  'aria-haspopup': {
+    control: { type: 'text' },
+    description:
+      'Indicates the availability and type of interactive popup element, such as menu or dialog, that can be triggered by an element.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'boolean | "menu" | "listbox" | "tree" | "grid" | "dialog"',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  'aria-controls': {
+    control: { type: 'text' },
+    description:
+      'Identifies the element (or elements) whose contents or presence are controlled by the current element.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'string',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  'aria-pressed': {
+    control: { type: 'boolean' },
+    description: 'Indicates the current "pressed" state of toggle buttons.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'boolean',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  type: {
+    control: { type: 'string' },
+    description: 'The behavior of the button when used in an HTML form.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: '"button" | "submit" | "reset""',
+      },
+      defaultValue: {
+        summary: '"button"',
+      },
+    },
+  },
+  excludeFromTabOrder: {
+    control: { type: 'boolean' },
+    description:
+      'Whether to exclude the element from the sequential tab order. If true, the element will not be focusable via the keyboard by tabbing. This should be avoided except in rare scenarios where an alternative means of accessing the element or its functionality via the keyboard is available.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'boolean',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  id: {
+    control: { type: 'text' },
+    description:
+      "The element's unique identifier. See [MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/id).",
     table: {
       category: 'React Aria - Button',
       type: {
@@ -291,8 +598,47 @@ const commonAriaButton = {
   },
   'aria-label': {
     control: { type: 'text' },
+    description: 'Defines a string value that labels the current element.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'string',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  'aria-labelledby': {
+    control: { type: 'text' },
+    description: 'Identifies the element (or elements) that labels the current element.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'string',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  'aria-describedby': {
+    control: { type: 'text' },
+    description: 'Identifies the element (or elements) that describes the object.',
+    table: {
+      category: 'React Aria - Button',
+      type: {
+        summary: 'string',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  'aria-details': {
+    control: { type: 'text' },
     description:
-      'From [AriaButtonProps](https://react-spectrum.adobe.com/react-aria/useButton.html). Sets the `aria-label` value.',
+      'Identifies the element (or elements) that provide a detailed, extended description for the object.',
     table: {
       category: 'React Aria - Button',
       type: {
@@ -370,6 +716,7 @@ export {
   commonAriaButton,
   commonAriaDialog,
   commonAriaFocusScope,
+  commonAriaHover,
   commonAriaModal,
   commonAriaOverlay,
   commonHTMLAttributes,
