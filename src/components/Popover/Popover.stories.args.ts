5f68ad781384dad3208ea10c0633e1b9d648b0c7
Christoph Scheffauer <scheffauerc@gmail.com>
2022-01-13 10:32:39 +0100

    refactor: several changes based on comments

--- src/components/Popover/Popover.stories.args.ts
+++ src/components/Popover/Popover.stories.args.ts
@@ -3,22 +3,56 @@ import { COLORS, PLACEMENTS } from './Popover.constants';
 
 export default {
   ...commonStyles,
+  trigger: {
+    description: `Determines the events that cause the Popover to show. <br/><br/>
+    Multiple event names should be separated by spaces. For example to allow both click and hover, use \`click mouseenter\` as the trigger.
+    <br/><br/>Possible event names: \`click\`, \`mouseenter\`, \`focusin\`, \`manual\` (to programmatically trigger the popover)
+    <br/>default: \`click\``,
+    control: { type: 'text' },
+    table: {
+      type: {
+        summary: 'string',
+      },
+    },
+  },
+  placement: {
+    description: `Placement of the Popover relative to the trigger component
+    <br/><br/>default \`bottom\``,
+    options: [...Object.values(PLACEMENTS as Record<string, string>)],
+    control: { type: 'select' },
+    table: {
+      type: {
+        summary: 'string',
+      },
+    },
+  },
+  showArrow: {
+    description: `Whether the arrow should be shown
+    <br/><br/>default \`true\``,
+    control: { type: 'boolean' },
+    table: {
+      type: {
+        summary: 'boolean',
+      },
+    },
+  },
+  interactive: {
+    description: `Determines if the Popover has interactive content inside of it,
+    so that it can be hovered over and clicked inside without hiding.
+    <br/><br/>default \`false\``,
+    control: { type: 'boolean' },
+    table: {
+      type: {
+        summary: 'boolean',
+      },
+    },
+  },
   children: {
     description: 'Provides the child nodes for this element.',
     control: { type: 'text' },
     table: {
       type: {
         summary: 'ReactNode',
-      },
-    },
-  },
-  placement: {
-    description: 'Placement of the popover relative to the trigger component',
-    options: [...Object.values(PLACEMENTS as Record<string, string>)],
-    control: { type: 'select' },
-    table: {
-      type: {
-        summary: 'string',
       },
     },
   },
