5ff8dd09b2cd58134d01d8978445193f5ec25c2a
Christoph Scheffauer <scheffauerc@gmail.com>
2022-01-13 16:52:56 +0100

    refactor: based on comments

--- src/components/Popover/Popover.constants.ts
+++ src/components/Popover/Popover.constants.ts
@@ -1,4 +1,5 @@
-import type { Round, Color, PlacementType, Elevation } from './Popover.types';
+import { PLACEMENTS } from '../ModalArrow/ModalArrow.constants';
+import type { Round, Color, Elevation } from './Popover.types';
 
 const CLASS_PREFIX = 'md-popover';
 
@@ -7,26 +8,6 @@ const COLORS: Record<string, Color> = {
   SECONDARY: 'secondary',
   TERTIARY: 'tertiary',
   QUATERNARY: 'quaternary',
-};
-
-const PLACEMENTS: Record<string, PlacementType> = {
-  TOP: 'top',
-  TOP_START: 'top-start',
-  TOP_END: 'top-end',
-  RIGHT: 'right',
-  RIGHT_START: 'right-start',
-  RIGHT_END: 'right-end',
-  BOTTOM: 'bottom',
-  BOTTOM_START: 'bottom-start',
-  BOTTOM_END: 'bottom-end',
-  LEFT: 'left',
-  LEFT_START: 'left-start',
-  LEFT_END: 'left-end',
-
-  // choose the side with most space:
-  AUTO: 'auto',
-  AUTO_START: 'auto-start',
-  AUTO_END: 'auto-end',
 };
 
 const ROUNDS: Record<number, Round> = {
@@ -56,6 +37,9 @@ const DEFAULTS = {
   ROUND: ROUNDS[0],
   ELEVATION: ELEVATIONS[0],
   COLOR: 'primary',
+  TRIGGER: 'click',
+  PLACEMENT: PLACEMENTS.BOTTOM as string,
+  SHOW_ARROW: true,
 };
 
 const STYLE = {
@@ -63,4 +47,7 @@ const STYLE = {
   arrowWrapper: `${CLASS_PREFIX}-arrow-wrapper`,
 };
 
-export { CLASS_PREFIX, DEFAULTS, STYLE, COLORS, PLACEMENTS, ELEVATIONS };
+// default offset between popover and trigger component:
+const OFFSET = 5;
+
+export { CLASS_PREFIX, DEFAULTS, STYLE, COLORS, ELEVATIONS, OFFSET };
