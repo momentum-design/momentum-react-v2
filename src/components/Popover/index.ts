64e6e6b3c2aa69fd9fa1e4f220d788fd1578214e
Timothy Scheuering <timsch@cisco.com>
2022-01-26 10:59:38 -0500

    feat(components/popover): add manual triggers

--- src/components/Popover/index.ts
+++ src/components/Popover/index.ts
@@ -1,9 +1,10 @@
 import { default as Popover } from './Popover';
 import * as CONSTANTS from './Popover.constants';
-import type { Props } from './Popover.types';
+import type { Props, PopoverInstance as Instance } from './Popover.types';
 
 export { CONSTANTS as POPOVER_CONSTANTS };
 
 export type PopoverProps = Props;
+export type PopoverInstance = Instance;
 
 export default Popover;
