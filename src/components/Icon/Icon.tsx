62c28f1cf17979bfcd532036b93f0e36ee953ddc
Catalin <ctorge@cisco.com>
2022-03-11 13:28:58 +0000

    feat(icon): revert strokeColor, fix space-list-item snaps

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -24,7 +24,7 @@ const Icon: React.FC<Props> = (props: Props) => {
     id,
     name,
     scale,
-    strokeColor = DEFAULTS.STROKE_COLOR,
+    strokeColor,
     style,
     weight,
     ...otherProps
