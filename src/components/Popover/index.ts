ca56de76574770c5b6ed5c8e5c2722c00181dc0f
Catalin <ctorge@cisco.com>
2022-06-09 11:08:40 +0100

    Revert "style(eslint): fix menu item"
    This reverts commit ef984b28af1dca99649c0e1b471d3939d8122383.

--- src/components/Popover/index.ts
+++ src/components/Popover/index.ts
@@ -1,17 +1,11 @@
 import { default as Popover } from './Popover';
 import * as CONSTANTS from './Popover.constants';
 
-import type {
-  Props,
-  PopoverInstance as Instance,
-  VariantType,
-  PlacementType,
-} from './Popover.types';
+import type { Props, PopoverInstance as Instance } from './Popover.types';
 
 export { CONSTANTS as POPOVER_CONSTANTS };
 
 export type PopoverProps = Props;
 export type PopoverInstance = Instance;
-export type { VariantType, PlacementType };
 
 export default Popover;
