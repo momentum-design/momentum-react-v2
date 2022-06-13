131b0eb85d6fbba87587e64e0a3de33e5fd011ee
breardon <breardon@cisco.com>
2022-06-12 23:40:47 -0700

    feat(chips-modifiers): add style tokens, remove extra colors, improve args -merge

--- src/components/Chip/Chip.stories.args.ts
+++ src/components/Chip/Chip.stories.args.ts
@@ -1,6 +1,5 @@
 import { commonStyles } from '../../storybook/helper.stories.argtypes';
 import { CHIP_CONSTANTS } from '.';
-import { TEAM_COLORS } from '../ThemeProvider/ThemeProvider.constants';
 
 const chipArgTypes = {
   children: {
