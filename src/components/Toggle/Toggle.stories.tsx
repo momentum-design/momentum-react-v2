cbaa11732ec97661a07941f99337052285721032
Laszlo Vadasz <maxinteger@gmail.com>
2022-06-16 00:30:41 +0100

    chore: simplify the toggle storybook page

--- src/components/Toggle/Toggle.stories.tsx
+++ src/components/Toggle/Toggle.stories.tsx
@@ -1,4 +1,3 @@
-import React, { FC, useCallback, useState } from 'react';
 import { MultiTemplate, Template } from '../../storybook/helper.stories.templates';
 import { DocumentationPage } from '../../storybook/helper.stories.docs';
 import StyleDocs from '../../storybook/docs.stories.style.mdx';
@@ -18,25 +17,13 @@ export default {
   },
 };
 
-const InteractiveExample: FC<ToggleProps> = (props: ToggleProps) => {
-  const [isSelected, setIsSelected] = useState(props.isSelected);
-  const onChangeHandler = useCallback(
-    (value) => {
-      props.onChange(value);
-      setIsSelected(value);
-    },
-    [props.onChange, setIsSelected]
-  );
-  return <Toggle {...props} isSelected={isSelected} onChange={onChangeHandler} />;
-};
-
 // NOTE: Primary story. This renders a single component with all external props.
-const Example = Template<ToggleProps>(InteractiveExample).bind({});
+const Example = Template<ToggleProps>(Toggle).bind({});
 
 Example.argTypes = { ...argTypes };
 
 Example.args = {
-  isSelected: TOGGLE_CONSTANTS.DEFAULTS.IS_SELECTED,
+  defaultSelected: TOGGLE_CONSTANTS.DEFAULTS.IS_SELECTED,
   isDisabled: TOGGLE_CONSTANTS.DEFAULTS.IS_DISABLED,
   label: 'Example text',
 };
@@ -45,7 +32,7 @@ Example.args = {
 const Common = MultiTemplate<ToggleProps>(Toggle).bind({});
 
 Common.argTypes = { ...argTypes };
-delete Common.argTypes.isSelected;
+delete Common.argTypes.defaultSelected;
 delete Common.argTypes.isDisabled;
 delete Common.argTypes.label;
 delete Common.argTypes.onChange;
@@ -53,10 +40,10 @@ delete Common.argTypes.onChange;
 Common.args = {};
 Common.parameters = {
   variants: [
-    { isSelected: true, isDisabled: false, label: 'Selected' },
-    { isSelected: true, isDisabled: true, label: 'Selected + Disabled' },
-    { isSelected: false, isDisabled: false, label: 'Not selected' },
-    { isSelected: false, isDisabled: true, label: 'Not selected + Disabled' },
+    { defaultSelected: true, isDisabled: false, label: 'Selected' },
+    { defaultSelected: true, isDisabled: true, label: 'Selected + Disabled' },
+    { defaultSelected: false, isDisabled: false, label: 'Not selected' },
+    { defaultSelected: false, isDisabled: true, label: 'Not selected + Disabled' },
   ],
 };
 
