7beb75fbcf0d59fe6a95a27f19f874ca2e927262
Ed Jex <ejex@cisco.com>
2022-07-19 14:44:52 +0000

    feat: wrote documentation + added styles

--- src/components/RadioGroup/RadioGroup.constants.ts
+++ src/components/RadioGroup/RadioGroup.constants.ts
@@ -1,9 +1,17 @@
-const CLASS_PREFIX = 'md-radio-group';
+const CLASS_PREFIX = 'md-radio';
 
-const DEFAULTS = {};
+const DEFAULTS = {
+  IS_DISABLED: false,
+  LABEL: undefined,
+};
 
 const STYLE = {
+  group: `${CLASS_PREFIX}-group`,
   wrapper: `${CLASS_PREFIX}-wrapper`,
+  selected: `${CLASS_PREFIX}-selected`,
+  notSelected: `${CLASS_PREFIX}-not-selected`,
+  focus: `${CLASS_PREFIX}-focus`,
+  icon: `${CLASS_PREFIX}-icon`,
 };
 
 export { CLASS_PREFIX, DEFAULTS, STYLE };
--- src/components/RadioGroup/RadioGroup.stories.args.ts
+++ src/components/RadioGroup/RadioGroup.stories.args.ts
@@ -1,12 +1,28 @@
 import { commonStyles } from '../../storybook/helper.stories.argtypes';
+import { DEFAULTS } from './RadioGroup.constants';
 
 const radioGroupArgTypes = {
   // NOTE: Below is an example. See [Storybook argTypes documentation]{@link https://storybook.js.org/docs/react/api/argtypes}.
   children: {
-    description: 'Provides the child nodes for this element.', // NOTE: Description of this prop.
-    control: { type: 'text' }, // NOTE: Control type for this prop.
     table: {
       disable: true,
+    },
+  },
+  label: {
+    description: 'The label of the checkbox group.',
+    table: {
+      type: {
+        summary: 'string',
+      },
+      defaultValue: DEFAULTS.LABEL,
+    },
+  },
+  options: {
+    description: 'Available options for the radio group',
+    table: {
+      type: {
+        summary: 'Array<RadioOption>',
+      },
     },
   },
 };
