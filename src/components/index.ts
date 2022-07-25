c01055bc23fb1d5c861ac8aba6227367bae93e0f
Ed Jex <ejex@cisco.com>
2022-07-25 08:54:56 +0000

    fix: properly exported radio group component

--- src/components/index.ts
+++ src/components/index.ts
@@ -58,3 +58,4 @@ export { default as Checkbox } from './Checkbox';
 export { default as ToastNotification } from './ToastNotification';
 export { default as Toggle } from './Toggle';
 export { default as Chip } from './Chip';
+export { default as RadioGroup, Radio } from './RadioGroup';
