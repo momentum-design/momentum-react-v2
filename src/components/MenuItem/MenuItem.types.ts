17cdb39bbe7be4b08aeac11f2ace16ccafc04b55
Shamayl Naaz <shnaaz@cisco.com>
2022-07-26 12:34:50 +0000

    fix(Menu): added title prop

--- src/components/MenuItem/MenuItem.types.ts
+++ src/components/MenuItem/MenuItem.types.ts
@@ -17,4 +17,9 @@ export interface Props<T> {
    * Handler to be called when this element is selected
    */
   onAction?: (key: Key) => void;
+
+  /**
+   * Title to show tooltip.
+   */
+  title?: string;
 }
