595b3fb2a9f94846f4e304c4b39929735f20bea4
Catalin <ctorge@cisco.com>
2022-07-28 16:06:26 +0100

    feat(font): add new font files

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -18,6 +18,7 @@ export default {
   },
   args: {
     children: 'Example Text',
+    style: { display: 'block', margin: '0.5rem 0' },
   },
 };
 
@@ -34,9 +35,41 @@ Types.parameters = {
     ...Object.values(TYPES).map((type) => {
       return {
         type,
+        children: type,
       };
     }),
   ],
 };
 
-export { Example, Types };
+const Fonts = MultiTemplate<TextProps>(Text).bind({});
+
+Fonts.argTypes = { ...argTypes };
+delete Types.argTypes.type;
+
+Fonts.parameters = {
+  variants: [
+    ...Object.values([
+      { weight: 100, style: 'normal', name: 'Thin' },
+      { weight: 100, style: 'italic', name: 'Thin' },
+      { weight: 200, style: 'normal', name: 'ExtraLight' },
+      { weight: 200, style: 'italic', name: 'ExtraLight' },
+      { weight: 300, style: 'normal', name: 'Light' },
+      { weight: 300, style: 'italic', name: 'Light' },
+      { weight: 'normal', style: 'normal', name: 'Regular' },
+      { weight: 'normal', style: 'italic', name: 'Regular' },
+      { weight: 500, style: 'normal', name: 'Medium' },
+      { weight: 500, style: 'italic', name: 'Medium' },
+      { weight: 'bold', style: 'normal', name: 'Bold' },
+      { weight: 'bold', style: 'italic', name: 'Bold' },
+      { weight: 800, style: 'normal', name: 'Heavy' },
+      { weight: 800, style: 'italic', name: 'Heavy' },
+    ]).map(({ weight, style, name }, index) => {
+      return {
+        style: { fontWeight: weight, fontStyle: style, marginTop: index % 2 == 0 ? '1rem' : 0 },
+        children: `CiscoSans ${name} - ${style}`,
+      };
+    }),
+  ],
+};
+
+export { Example, Types, Fonts };
