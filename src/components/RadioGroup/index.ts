c515883998eab0e23cce2b06543a1c552271ee8b
Edward Jex <edwardjex@live.co.uk>
2022-08-02 10:02:50 +0100

    fix: simplified exports
    Co-authored-by: László Vadász <maxinteger@gmail.com>

--- src/components/RadioGroup/index.ts
+++ src/components/RadioGroup/index.ts
@@ -1,9 +1,4 @@
 import RadioGroup from './RadioGroup';
-import * as CONSTANTS from './RadioGroup.constants';
-import { RadioProps as _RadioProps, GroupProps } from './RadioGroup.types';
-
-export { CONSTANTS as RADIO_GROUP_CONSTANTS };
-
-export type RadioProps = _RadioProps;
-export type RadioGroupProps = GroupProps;
+export * as RADIO_GROUP_CONSTANTS from './RadioGroup.constants';
+export * from './RadioGroup.types';
 export default RadioGroup;
