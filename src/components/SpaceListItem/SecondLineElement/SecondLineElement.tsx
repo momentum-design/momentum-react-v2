67d6e3975479e850ce6f928ca721217a434853fb
Jordan Rowan <jrowan645@outlook.com>
2022-09-07 18:02:47 +0100

    fix(user-divider-dot): moved subcomponent to subfolder and addressed comments

--- src/components/SpaceListItem/SecondLineElement/SecondLineElement.tsx
+++ src/components/SpaceListItem/SecondLineElement/SecondLineElement.tsx
@@ -1,20 +1,21 @@
 import React, { FC } from 'react';
 
-import { DEFAULTS } from './SpaceListItem.constants';
-import { SecondLineElementProps as Props } from './SpaceListItem.types';
-import DividerDot from '../DividerDot';
+import { DEFAULTS } from './SecondLineElement.constants';
+import { Props } from './SecondLineElement.types';
+import DividerDot from '../../DividerDot';
 
 /**
  * The SecondLineElement subcomponent is used only in SpaceListItem to
  * render the secondLine array with DividerDots inbetween.
+ * @internal use only. Should not be consumed from any external parties.
  */
 const SecondLineElement: FC<Props> = (props: Props) => {
-  const { children, showDividerDot = DEFAULTS.DIVIDER_DOT } = props;
+  const { children, showDividerDot = DEFAULTS.SHOW_DIVIDER_DOT } = props;
 
   return (
     <>
       {showDividerDot && <DividerDot data-test="multiple-string-second-line-divider-dot" />}
-      {children}
+      <span data-test="second-line-element-child">{children}</span>
     </>
   );
 };
