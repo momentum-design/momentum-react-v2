68acca56dd0f4238ae5265f91742e033105aa49c
ndelmar <ndelmar@cisco.com>
2023-01-27 18:42:41 +0000

    feat(tokens-checker): address comments 2

--- src/helpers/cssTokenCheck.js
+++ src/helpers/cssTokenCheck.js
@@ -1,10 +1,17 @@
 const fs = require('fs'); // eslint-disable-line
 const glob = require('glob'); // eslint-disable-line
-const { getMDSTokens, getMDLTokens } = require('./getTokenCssVars'); // eslint-disable-line
+const { getTokensFromSource } = require('./getTokenCssVars'); // eslint-disable-line
 
 let mdlTokens;
 let mdsTokens;
-let isSrc;
+// MDSCheck is one in which we make sure all tokens used in a repo come only from @momentum-design package
+let isMDSCheck;
+let errors = [];
+
+const variableTokens = new Set([
+  '--mds-color-theme-text-${level}-normal',
+  '--mds-color-theme-text-team${teamColorForToken}-normal',
+]);
 
 const isMDSTokenValid = function (token) {
   if (!mdsTokens[token]) {
@@ -13,7 +20,7 @@ const isMDSTokenValid = function (token) {
 };
 
 const isMDLTokenValid = function (token) {
-  if (isSrc) {
+  if (isMDSCheck) {
     throw new Error(`Legacy MDL token ${token} was found in src/`);
   }
   if (!mdlTokens[token]) {
@@ -37,89 +44,67 @@ const isVarTokenValid = function (token) {
   if (!mdsTokens[token] && !mdlTokens[token]) {
     throw new Error(`Var token not found for ${token}`);
   }
+
+  if (isMDSCheck && !mdsTokens[token]) {
+    throw new Error(`${token} was found in src/, where only --mds tokens are allowed`);
+  }
 };
 
-const checkMDSTokens = function (source) {
+// source (String): the raw content of each evaluated file
+// tokenType (String): could be 'mds', 'mdl', or 'var'
+const checkTokens = function (source, tokenType) {
   const lines = source.split('\n');
 
-  const variableTokens = new Set([
-    '--mds-color-theme-text-${level}-normal',
-    '--mds-color-theme-text-team${teamColorForToken}-normal',
-  ]);
-
   lines.forEach((currentLine, index) => {
-    const matcher = /--mds(.*?)[,;)]/g;
-    const matches = currentLine.matchAll(matcher);
+    let matcher;
+    let sliceFrom;
+    let sliceUntil;
+    switch (tokenType) {
+      case 'mds':
+        //match[0] gives you the entire match string.
+        //Example: In 'background-color: var(--mds-color-theme-common-overlay-primary-normal);'
+        //match[0] gives you '--mds-color-theme-common-overlay-primary-normal)'
+        //With slice(0,-1), we cut the last char from that string to remove the final ')'
+        matcher = /--mds(.*?)[,;)]/g;
+        sliceFrom = 0;
+        sliceUntil = -1;
+        break;
+      case 'mdl':
+        //match[0] gives you the entire match string.
+        //Example: In '$bg-css-var: --mdl-button-secondary-bg-color,'
+        //match[0] gives you '--mdl-button-secondary-bg-color,'
+        //With slice(0,-1), we cut the last char from that string to remove the final ',' or ')'
+        matcher = /--mdl(.*?)[,;)]/g;
+        sliceFrom = 0;
+        sliceUntil = -1;
+        break;
+      case 'var':
+        //match[0] gives you the entire match string.
+        //Example 1: In 'background-color: var(--mdl-background-quaternary, rgba(0, 115, 149, 0.56)); }'
+        //match[0] gives you 'var(--mdl-background-quaternaryss, rgba(0, 115, 149, 0.56)'
+        //I do not want to test the rgba colors, so I replace any match of them with '**'
+        //Which gives me 'var(--mdl-background-quaternaryss, **'
+        //With slice(4,-1), we cut the 'var(' in the beginning and the last char '*'
+        //So get get '--mdl-background-quaternaryss, *'. If we split that with ', '
+        //We get an array like [ '--mdl-background-quaternaryss', '*' ]
 
-    for (const match of matches) {
-      //match[0] gives you the entire match string.
-      //Example: In 'background-color: var(--mds-color-theme-common-overlay-primary-normal);'
-      //match[0] gives you '--mds-color-theme-common-overlay-primary-normal)'
-      //With slice(0,-1), we cut the last char from that string to remove the final ')'
-      const token = match[0].slice(0, -1);
-      if (!variableTokens.has(token)) {
-        try {
-          isMDSTokenValid(token);
-        } catch (error) {
-          throw new Error(`line ${index + 1}: ${error}`);
-        }
-      }
+        //Example 2: In 'color: var(--mds-color-theme-text-success-normal);'
+        //match[0] gives you 'var(--mds-color-theme-text-success-normal)'
+        //I do not find any rgba color in this var so the replacement for '**' is ignored.
+        //With slice(4,-1), we cut the 'var(' in the beginning and the last char ','
+        //So get get '-mds-color-theme-text-success-normal'. If we split that with ', '
+        //We get an array like [ '-mds-color-theme-text-success-normal']
+        matcher = /var[(](.*?)[)]/g;
+        sliceFrom = 4;
+        sliceUntil = -1;
+        break;
+      default:
+        throw new Error(`Invalid token type ${tokenType}`);
     }
-  });
-};
-
-const checkMDLTokens = function (source) {
-  const lines = source.split('\n');
-
-  lines.forEach((currentLine, index) => {
-    const matcher = /--mdl(.*?)[,;)]/g;
     const matches = currentLine.matchAll(matcher);
 
     for (const match of matches) {
-      //match[0] gives you the entire match string.
-      //Example: In '$bg-css-var: --mdl-button-secondary-bg-color,'
-      //match[0] gives you '--mdl-button-secondary-bg-color,'
-      //With slice(0,-1), we cut the last char from that string to remove the final ',' or ')'
-      const token = match[0].slice(0, -1);
-      try {
-        isMDLTokenValid(token);
-      } catch (error) {
-        throw new Error(`line ${index + 1}: ${error}`);
-      }
-    }
-  });
-};
-
-const checkVarTokens = function (source) {
-  const lines = source.split('\n');
-
-  const variableTokens = new Set([
-    '--mds-color-theme-text-${level}-normal',
-    '--mds-color-theme-text-team${teamColorForToken}-normal',
-  ]);
-
-  lines.forEach((currentLine, index) => {
-    const matcher = /var[(](.*?)[)]/g;
-    const matches = currentLine.matchAll(matcher);
-
-    for (const match of matches) {
-      //match[0] gives you the entire match string.
-
-      //Example 1: In 'background-color: var(--mdl-background-quaternary, rgba(0, 115, 149, 0.56)); }'
-      //match[0] gives you 'var(--mdl-background-quaternaryss, rgba(0, 115, 149, 0.56)'
-      //I do not want to test the rgba colors, so I replace any match of them with '**'
-      //Which gives me 'var(--mdl-background-quaternaryss, **'
-      //With slice(4,-1), we cut the 'var(' in the beginning and the last char '*'
-      //So get get '--mdl-background-quaternaryss, *'. If we split that with ', '
-      //We get an array like [ '--mdl-background-quaternaryss', '*' ]
-
-      //Example 2: In 'color: var(--mds-color-theme-text-success-normal);'
-      //match[0] gives you 'var(--mds-color-theme-text-success-normal)'
-      //I do not find any rgba color in this var so the replacement for '**' is ignored.
-      //With slice(4,-1), we cut the 'var(' in the beginning and the last char ','
-      //So get get '-mds-color-theme-text-success-normal'. If we split that with ', '
-      //We get an array like [ '-mds-color-theme-text-success-normal']
-      let tokens = match[0].replace(/rgba[(](.*?)[)]/g, '**').slice(4, -1);
+      const tokens = match[0].replace(/rgba[(](.*?)[)]/g, '**').slice(sliceFrom, sliceUntil);
       tokens.split(', ').forEach((token) => {
         if (
           !variableTokens.has(token) &&
@@ -128,7 +113,9 @@ const checkVarTokens = function (source) {
           !token.includes('inherit')
         ) {
           try {
-            isVarTokenValid(token);
+            tokenType === 'mds' && isMDSTokenValid(token);
+            tokenType === 'mdl' && isMDLTokenValid(token);
+            tokenType === 'var' && isVarTokenValid(token);
           } catch (error) {
             throw new Error(`line ${index + 1}: ${error}`);
           }
@@ -140,15 +127,13 @@ const checkVarTokens = function (source) {
 
 const readAndCheckTokens = function (filePath) {
   let data = fs.readFileSync(filePath, 'utf8');
-  checkMDSTokens(data);
-  checkMDLTokens(data);
-  checkVarTokens(data);
+  checkTokens(data, 'mds');
+  checkTokens(data, 'mdl');
+  checkTokens(data, 'var');
 };
 
-const checkAllSrcFiles = function () {
-  isSrc = true;
-  glob('src/**/*.*', {}, (_er, files) => {
-    const errors = [];
+const checkAllFiles = function (pathPattern) {
+  glob(pathPattern, {}, (_er, files) => {
     files.forEach((filePath) => {
       try {
         if (
@@ -162,66 +147,40 @@ const checkAllSrcFiles = function () {
       }
     });
     if (errors.length) {
-      throw new Error('Missing CSS vars detected in the following:\n\n' + errors.join('\n'));
+      throw new Error('Design token error detected in the following:\n\n' + errors.join('\n'));
     }
   });
-  isSrc = false;
-};
-
-const checkAllSCSSFiles = function () {
-  isSrc = false;
-  glob('scss/**/*.*', {}, (_er, files) => {
-    const errors = [];
-    files.forEach((filePath) => {
-      try {
-        readAndCheckTokens(filePath);
-      } catch (error) {
-        errors.push(`In file ${filePath}:\n ${error}\n`);
-      }
-    });
-    if (errors.length) {
-      throw new Error('Missing CSS vars detected in the following:\n\n' + errors.join('\n'));
-    }
-  });
-  isSrc = false;
-};
-
-const checkAllCSSFiles = function () {
-  isSrc = false;
-  glob('css/**/*.*', {}, (_er, files) => {
-    const errors = [];
-    files.forEach((filePath) => {
-      try {
-        readAndCheckTokens(filePath);
-      } catch (error) {
-        errors.push(`In file ${filePath}:\n ${error}\n`);
-      }
-    });
-    if (errors.length) {
-      throw new Error('Missing CSS vars detected in the following:\n\n' + errors.join('\n'));
-    }
-  });
-  isSrc = false;
 };
 
 const args = process.argv.slice(2);
 
 const file = args[0];
+isMDSCheck = args[1] || false;
+const globPatterns = args[2] || [];
 
-getMDSTokens().then((resultMdsTokens) => {
+getTokensFromSource(
+  '../../node_modules/@momentum-design/tokens/dist/css/theme/webex/light-stable.css',
+  'mds-css-stable'
+).then((resultMdsTokens) => {
   mdsTokens = resultMdsTokens;
 
-  getMDLTokens().then((resultMdlTokens) => {
-    mdlTokens = resultMdlTokens;
+  getTokensFromSource('../../src/examples/ThemeSelect/themes.js', 'mdl-examples').then(
+    (resultMdlTokens) => {
+      mdlTokens = resultMdlTokens;
 
-    if (file) {
-      isSrc = file.includes('src/') ? true : false;
-      readAndCheckTokens(file);
-      isSrc = false;
-    } else {
-      checkAllSrcFiles();
-      checkAllSCSSFiles();
-      checkAllCSSFiles();
+      if (file) {
+        readAndCheckTokens(file);
+      } else if (globPatterns.length) {
+        globPatterns.forEach((glob) => {
+          checkAllFiles(glob);
+        });
+      } else {
+        isMDSCheck = true;
+        checkAllFiles('src/**/*.*');
+        isMDSCheck = false;
+        checkAllFiles('scss/**/*.*');
+        checkAllFiles('css/**/*.*');
+      }
     }
-  });
+  );
 });
--- src/helpers/getTokenCssVars.js
+++ src/helpers/getTokenCssVars.js
@@ -1,45 +1,51 @@
 const path = require('path'); // eslint-disable-line
 const fs = require('fs/promises'); // eslint-disable-line
 
-const getMDSTokens = function () {
+// sourcePath is the path of the source you want to get the valid tokens from. Example: '../../node_modules/@momentum-design/tokens/dist/css/theme/webex/light-stable.css'
+// sourceType can be 'mds-css-stable' or 'mdl-examples'
+
+const getTokensFromSource = function (sourcePath, sourceType) {
   return fs
-    .readFile(
-      path.join(
-        __dirname,
-        '../../node_modules/@momentum-design/tokens/dist/css/theme/webex/light-stable.css'
-      )
-    )
+    .readFile(path.join(__dirname, sourcePath))
     .then((buffer) => buffer.toString('utf-8'))
-    .then((data) =>
-      data
-        .split('\n')
-        .filter((line) => line.includes('--'))
-        .map((line) => line.replace(';', '"').replace(': ', '": "').replace('--', '"--'))
-        .join(',\n')
-    )
+    .then((data) => data.split('\n').filter((line) => line.includes('--')))
+    .then((data) => {
+      switch (sourceType) {
+        case 'mds-css-stable':
+          // To get a string that is parseable to json from @momentum-design/tokens/dist/css/theme/webex/light-stable.css
+          // 1. We want to replace ';' with '"'. From '--mds-color-theme-common-text-white: #fffffff2;' we get '--mds-color-theme-common-text-white: #fffffff2"'
+          // 2. We want to replace ': ' with '": ". From '--mds-color-theme-common-text-white: #fffffff2"' we get '--mds-color-theme-common-text-white": "#fffffff2"'
+          // 3. We want to replace '--' with '"--'. From '--mds-color-theme-common-text-white": "#fffffff2"' we get '"--mds-color-theme-common-text-white": "#fffffff2"'
+          // 4. We want to put a ,\n at the end of each line, and join them to return a big string that looks like this:
+          //
+          // "--mds-color-theme-common-text-white": "#fffffff2",
+          // "--mds-color-theme-common-text-white": "#fffffff2",
+          // ...
+          return data
+            .map((line) => line.replace(';', '"').replace(': ', '": "').replace('--', '"--'))
+            .join(',\n');
+        case 'mdl-examples':
+          // To get a string that is parseable to json from src/examples/ThemeSelector/themes.js
+          // 1. We want to replace "'", '"'. From ''--mdl-button-primary-bg-color': '#007AA3',', we get '"--mdl-button-primary-bg-color': '#007AA3',',
+          // 2. We want to replace '': ' with '": ". From '"--mdl-button-primary-bg-color': '#007AA3',' we get '"--mdl-button-primary-bg-color": "#007AA3','
+          // 3. We want to replace '',' with '",'. From '"--mdl-button-primary-bg-color": "#007AA3',' we get '"--mdl-button-primary-bg-color": "#007AA3",'
+          // 5. We want to put a \n at the end of each line, and join them to form a big string
+          // 4. We want to cut the last char of the big string because it is a ','. In json, the last member should haveno ',' at the end.
+          //
+          // "--mdl-button-primary-bg-color": "#007AA3",
+          // "--mdl-button-primary-bg-color": "#007AA3",
+          // ...
+          // "--mdl-button-primary-bg-color": "#007AA3"
+          return data
+            .map((line) => line.replace("'", '"').replace("': '", '": "').replace("',", '",'))
+            .join('\n')
+            .slice(0, -1);
+        default:
+          throw new Error('Invalid sourceType when trying to getTokensFromSource');
+      }
+    })
     .then((formattedLines) => ['{', formattedLines, '}'].join('\n'))
     .then((jsonString) => JSON.parse(jsonString));
 };
 
-const getMDLTokens = function () {
-  return fs
-    .readFile(path.join(__dirname, '../../src/examples/ThemeSelect/themes.js'))
-    .then((buffer) => buffer.toString('utf-8'))
-    .then((data) =>
-      data
-        .split('\n')
-        .filter((line) => line.includes('--'))
-        .map((line) =>
-          line.replace("'", '"').replace("': '", '": "').replace("',", '",').replace("'", '"')
-        )
-        .join('\n')
-    )
-    .then((formattedLines) => {
-      // We take out the last char of the last line, which is a ',' comma.
-      // We need to take this out in order to make the json valid
-      return ['{', formattedLines.slice(0, -1), '}'].join('\n');
-    })
-    .then((jsonString) => JSON.parse(jsonString));
-};
-
-module.exports = { getMDSTokens, getMDLTokens };
+module.exports = { getTokensFromSource };
