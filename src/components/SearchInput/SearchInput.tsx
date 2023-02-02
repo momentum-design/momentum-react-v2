0f0f363df46b51533db252adf66dca4074371c29
Deepak Rohilla <drohilla@cisco.com>
2023-02-02 16:35:35 +0530

    fix(test): search input close button tabindex test

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -77,6 +77,7 @@ const SearchInput = (props: Props, ref: RefObject<HTMLInputElement>): ReactEleme
           className={STYLE.clear}
           {...clearButtonProps}
           aria-label={clearButtonAriaLabel}
+          tabIndex={-1}
         >
           <Icon weight="bold" scale={ICON_HEIGHT_MAPPING[height]} name="cancel" />
         </ButtonSimple>
