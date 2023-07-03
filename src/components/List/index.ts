2eead5f949477ae9caa1b0cb0f3597b19a1d2af1
Laszlo Vadasz <maxinteger@gmail.com>
2023-07-03 15:03:29 +0100

    chore: export useListContext hook
    With the useListContext integrator can access to the list internal state
    (e.g. selected row)

--- src/components/List/index.ts
+++ src/components/List/index.ts
@@ -4,6 +4,8 @@ import { Props } from './List.types';
 
 export { CONSTANTS as LIST_CONSTANTS };
 
+export { useListContext } from './List.utils';
+
 export type ListProps = Props;
 
 export default List;
