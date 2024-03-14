75f5117fdb75b5e952914171fd3d725635805f21
tiahan <tiahan@cisco.com>
2024-03-14 14:49:38 +0800

    fix(combobox): reslove ci test issue

--- src/components/ComboBox/ComboBox.utils.ts
+++ src/components/ComboBox/ComboBox.utils.ts
@@ -30,7 +30,7 @@ export const getSumScrollTop = (element:Element):number => {
     while (element) {
         element = element.parentElement;
         if (element) {
-            const scrollTop = element.scrollTop ?? 0;
+            const scrollTop = Number.isNaN(element.scrollTop) ? 0 : element.scrollTop;
             sum += scrollTop ;
         }
     }
