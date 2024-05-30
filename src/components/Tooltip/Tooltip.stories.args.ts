92fda82baa7bbda488b90041e853fbda0757dfb6
Laszlo Vadasz <lvadasz@cisco.com>
2024-05-30 15:29:30 +0100

    feat: add new tooltip component

---
+++ src/components/Tooltip/Tooltip.stories.args.ts
@@ -0,0 +1,112 @@
+import { commonStyles } from '../../storybook/helper.stories.argtypes';
+import { PLACEMENTS } from '../ModalArrow/ModalArrow.constants';
+import { COLORS } from '../ModalContainer/ModalContainer.constants';
+import { DEFAULTS } from './Tooltip.constants';
+import { BOUNDARIES } from '../Popover/Popover.constants';
+
+const popoverArgTypes = {
+  isDescendant: {
+    description: `Whether the tooltips be a description or a label of the trigger component`,
+    control: { type: 'boolean' },
+    table: {
+      type: {
+        summary: 'boolean',
+      },
+      defaultValue: {
+        summary: DEFAULTS.IS_DESCRIPTION,
+      },
+    },
+  },
+  placement: {
+    description: `Placement of the Tooltip relative to the trigger component`,
+    options: [...Object.values(PLACEMENTS as Record<string, string>)],
+    control: { type: 'select' },
+    table: {
+      type: {
+        summary: 'string',
+      },
+      defaultValue: {
+        summary: DEFAULTS.PLACEMENT,
+      },
+    },
+  },
+  offsetSkidding: {
+    description: `The offset skidding (in px) along the reference.`,
+    control: { type: 'number' },
+    table: {
+      type: {
+        summary: 'number',
+      },
+      defaultValue: {
+        summary: DEFAULTS.OFFSET_SKIDDING,
+      },
+    },
+  },
+  offsetDistance: {
+    description: `The offset distance (in px) from the reference.`,
+    control: { type: 'number' },
+    table: {
+      type: {
+        summary: 'number',
+      },
+      defaultValue: {
+        summary: DEFAULTS.OFFSET_DISTANCE,
+      },
+    },
+  },
+  variant: {
+    description: `Variant of the Tooltip - can be either small or medium`,
+    options: ['small', 'medium'],
+    control: { type: 'select' },
+    table: {
+      type: {
+        summary: 'string',
+      },
+      defaultValue: {
+        summary: DEFAULTS.VARIANT,
+      },
+    },
+  },
+  children: {
+    description: 'Provides the child nodes for this element.',
+    control: { type: 'text' },
+    table: {
+      type: {
+        summary: 'ReactNode',
+      },
+    },
+  },
+  color: {
+    description: 'What color to render this `<Tooltip />` as.',
+    control: { type: 'select' },
+    options: [...Object.values(COLORS)],
+    table: {
+      type: {
+        summary: 'string',
+      },
+      defaultValue: {
+        summary: DEFAULTS.COLOR,
+      },
+    },
+  },
+  boundary: {
+    description: 'The overflow boundary of the tooltip element.',
+    control: { type: 'select' },
+    options: [undefined, ...Object.values(BOUNDARIES)],
+    table: {
+      type: {
+        summary: 'string',
+      },
+      defaultValue: {
+        summary: DEFAULTS.BOUNDARY,
+      },
+    },
+  },
+};
+
+export { popoverArgTypes };
+
+export default {
+  ...commonStyles,
+  ...popoverArgTypes,
+};
---
+++ src/components/Tooltip/Tooltip.tsx
@@ -0,0 +1,45 @@
+import React, { ForwardedRef, forwardRef, useCallback } from 'react';
+import Popover, { PopoverInstance } from '../Popover';
+import { Props } from './Tooltip.types';
+import { DEFAULTS } from './Tooltip.constants';
+
+const Tooltip = forwardRef(
+  (
+    { isDescription = DEFAULTS.IS_DESCRIPTION, triggerComponent, children, ...otherProps }: Props,
+    ref: ForwardedRef<HTMLElement>
+  ) => {
+    // Update aria props manually, because
+    // "The `aria` attribute is reserved for future use in React."
+    const updateInstance = useCallback(
+      (popoverInstance: PopoverInstance | undefined) => {
+        popoverInstance?.setProps({
+          aria: {
+            expanded: false,
+            content: isDescription ? 'describedby' : 'labelledby',
+          },
+        });
+      },
+      [isDescription]
+    );
+
+    return (
+      <Popover
+        ref={ref}
+        interactive={false}
+        trigger="mouseenter focus"
+        triggerComponent={triggerComponent}
+        showArrow
+        addBackdrop={false}
+        setInstance={updateInstance}
+        role="tooltip"
+        {...otherProps}
+      >
+        {children}
+      </Popover>
+    );
+  }
+);
+
+Tooltip.displayName = 'Tooltip';
+
+export default Tooltip;
