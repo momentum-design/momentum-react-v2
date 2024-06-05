af5ae34dfc41a29707ef43246ee5334d94a74678
Judy Zhu (judyz) <judyz@cisco.com>
2024-06-05 17:27:38 +0800

    fix(accessibility): add alt for img

--- src/legacy/Lightbox/tests/index.spec.js
+++ src/legacy/Lightbox/tests/index.spec.js
@@ -14,6 +14,7 @@ describe('tests for <Lightbox />', () => {
       <Lightbox
         applicationId="app"
         name="test"
+        alt="test"
         height={100}
         width={100}
         pages={[
