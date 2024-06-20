88c394be3e40fe04b44fe62fb5a1ee4fcbc73248
Jordan Rowan <jrowan645@outlook.com>
2024-06-20 19:31:17 +0100

    fix(OverlayAlert): don't close overlay alert if popover is open inside

--- src/components/Popover/tippy-plugins/hideOnEscPlugin.ts
+++ src/components/Popover/tippy-plugins/hideOnEscPlugin.ts
@@ -1,5 +1,7 @@
 import type { Instance as TippyInstance, Plugin } from 'tippy.js';
 
+import {dispatchEvent, EventType} from '../Popover.events';
+
 const openedTippyInstances: TippyInstance[] = [];
 
 // hide the last opened popover when Escape key is pressed
@@ -26,6 +28,13 @@ const addInstance = (instance: TippyInstance) => {
   }
 
   openedTippyInstances.push(instance);
+
+  // Send custom event that tippy instance is shown on screen. This event is listened for in instances of OverlayAlert to prevent it from closing unintentionally.
+  //
+  // We do this in the next event cycle because, if we don't, and the first element focused in an OverlayAlert is the trigger of a tooltip, then the event is
+  // dispatched before the OverlayAlert is mounted. (Use setTimeout without delay to execute code in the
+  // next event cycle, see https://developer.mozilla.org/en-US/docs/Web/API/setTimeout#delay)
+  setTimeout(() => dispatchEvent(EventType.TIPPY_INSTANCE_ADDED, instance));
 };
 
 /**
@@ -38,6 +47,9 @@ const removeInstance = (instance: TippyInstance) => {
   if (openedTippyInstances.length === 0) {
     window.removeEventListener('keydown', onKeyDown);
   }
+  // Send custom event that tippy instance is no longer shown on screen. This event is listened for in instances of OverlayAlert to allow it to close once there are no tippy instances
+  // still shown on screen that were opened after the render of the OverlayAlert.
+  dispatchEvent(EventType.TIPPY_INSTANCE_REMOVED, instance);
 };
 
 /**
