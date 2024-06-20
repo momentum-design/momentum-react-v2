fd130c535f1cd188dad10d69b97b0634144922a3
Jing Wang <jiwang7@cisco.com>
2024-06-20 14:17:29 +0800

    feat: add two attribute to render Icon in the right of link.

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -5,35 +5,39 @@ import { DEFAULTS, STYLE } from './Link.constants';
 import { Props } from './Link.types';
 import './Link.style.scss';
 import classnames from 'classnames';
+import Icon from '../Icon';
 
 const Link = forwardRef((props: Props, providedRef: RefObject<HTMLAnchorElement>) => {
-  const { className, title } = props;
+  const { className, title, isWithIcon, iconProps, ...restProps } = props;
   const internalRef = useRef();
   const ref = providedRef || internalRef;
 
   const mutatedProps = {
-    ...props,
+    ...restProps,
+    title,
     isDisabled: props.disabled,
     isInverted: props.inverted,
   };
 
   delete mutatedProps.disabled;
-  delete mutatedProps.className;
 
   const { linkProps } = useLink({ ...mutatedProps, elementType: 'a' }, ref);
 
   return (
     <FocusRing disabled={props.disabled}>
-      <a
-        className={classnames(STYLE.wrapper, className)}
-        {...linkProps}
-        ref={ref}
-        data-disabled={props.disabled || DEFAULTS.DISABLED}
-        data-inverted={props.inverted || DEFAULTS.INVERTED}
-        title={title}
-      >
-        {props.children}
-      </a>
+      <div className={STYLE.container}>
+        <a
+          className={classnames(STYLE.wrapper, className)}
+          {...linkProps}
+          ref={ref}
+          data-disabled={props.disabled || DEFAULTS.DISABLED}
+          data-inverted={props.inverted || DEFAULTS.INVERTED}
+          title={title}
+        >
+          {props.children}
+        </a>
+        {isWithIcon && <Icon className={STYLE.icon} scale={16} name="pop-out" {...iconProps} />}
+      </div>
     </FocusRing>
   );
 });
