bcc5edc1531ef18ab0f12cecbe0536137bb37c77
Catalin <ctorge@cisco.com>
2024-07-09 17:16:12 +0100

    feat(aria-toolbar-item): address pr comments

---
+++ src/utils/useProvidedRef.test.ts
@@ -0,0 +1,26 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useProvidedRef } from './useProvidedRef';
+
+describe('useProvidedRef', () => {
+  it('should return an internal ref with initial value if no ref is provided', () => {
+    const { result } = renderHook(() => useProvidedRef(null, 'initial'));
+    expect(result.current.current).toBe('initial');
+  });
+
+  it('should use the provided object ref', () => {
+    const providedRef = { current: 'provided' };
+    const { result } = renderHook(() => useProvidedRef(providedRef));
+
+    expect(result.current).toBe(providedRef);
+    expect(result.current.current).toBe('provided');
+  });
+
+  it('should call the provided function ref', () => {
+    const refCallback = jest.fn();
+    const { result } = renderHook(() => useProvidedRef(refCallback, 'initial'));
+
+    expect(refCallback).toHaveBeenCalledWith('initial');
+    expect(result.current).toBe({ current: 'initial' });
+    expect(result.current.current).toBe('initial');
+  });
+});
