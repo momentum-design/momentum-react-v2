a5e57c68083dee0c5acf0ad7e57cb9b45b0d426e
Jing Wang <jiwang7@cisco.com>
2024-07-12 14:21:45 +0800

    fix: typo

--- src/components/Link/Link.types.ts
+++ src/components/Link/Link.types.ts
@@ -45,7 +45,7 @@ export interface Props extends AriaLinkProps {
 
   /**
    * icon props
-   * When use iconProps isWithIcon is necessary
+   * When use iconProps hasExternalLinkIcon is necessary
    */
   iconProps?: Partial<IconProps>;
 
