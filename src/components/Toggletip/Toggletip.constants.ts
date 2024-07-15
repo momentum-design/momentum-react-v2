2bdd161aaf133dd5ef753e6dc4a2933780a68813
Laszlo Vadasz <lvadasz@cisco.com>
2024-07-15 15:01:16 +0100

    fix: re-factor toggletip tip
    - replaced non-interactive popover with interactive one
    - removed the live aria
    
    issue: https://jira-eng-gpk2.cisco.com/jira/browse/SPARK-502450

--- src/components/Toggletip/Toggletip.constants.ts
+++ src/components/Toggletip/Toggletip.constants.ts
@@ -2,12 +2,18 @@ import { PLACEMENTS } from '../ModalArrow/ModalArrow.constants';
 import { COLORS } from '../ModalContainer/ModalContainer.constants';
 import { BOUNDARIES } from '../Popover/Popover.constants';
 
+const CLASS_PREFIX = 'md-toggletip';
+
 export const DEFAULTS = {
   BOUNDARY: BOUNDARIES.PARENT,
   COLOR: COLORS.PRIMARY,
   OFFSET_DISTANCE: 5,
   OFFSET_SKIDDING: 0,
-  PLACEMENT: PLACEMENTS.AUTO as string,
+  PLACEMENT: PLACEMENTS.BOTTOM as string,
   STRATEGY: 'absolute' as const,
   VARIANT: 'small',
 } as const;
+
+export const STYLE = {
+  wrapper: `${CLASS_PREFIX}-wrapper`,
+};
