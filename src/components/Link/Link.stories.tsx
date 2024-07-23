5a1bb8955b155ac3df855b97ccb28a09877ce102
Jing Wang <jiwang7@cisco.com>
2024-07-23 14:26:08 +0800

    feat: add LinkNext

---
+++ src/components/Link/Link.stories.tsx
@@ -0,0 +1,142 @@
+import { MultiTemplateWithPseudoStates, Template } from '../../storybook/helper.stories.templates';
+import { DocumentationPage } from '../../storybook/helper.stories.docs';
+import StyleDocs from '../../storybook/docs.stories.style.mdx';
+import Link, { LinkProps } from './';
+import argTypes from './Link.stories.args';
+import Documentation from './Link.stories.docs.mdx';
+import Icon from '../Icon';
+import Text from '../Text';
+import React from 'react';
+import {omit} from 'lodash';
+
+export default {
+  title: 'Momentum UI/Link',
+  component: Link,
+  parameters: {
+    expanded: true,
+    docs: {
+      page: DocumentationPage(Documentation, StyleDocs),
+    },
+  },
+  args: {
+    children: 'Example Text',
+  },
+};
+
+const Example = Template<LinkProps>(Link).bind({});
+
+Example.args = {
+  href: 'https://www.webex.com/',
+  target: '_blank',
+};
+
+Example.argTypes = { ...argTypes };
+
+const LinkContainer = (props) => {
+  if (props.inverted) {
+    return (
+      <div style={{ backgroundColor: 'var(--mds-color-theme-teaching-background-normal)' }}>
+        <Link {...props} />
+      </div>
+    );
+  } else {
+    return <Link {...props} />;
+  }
+};
+
+const States = MultiTemplateWithPseudoStates<LinkProps>(LinkContainer).bind({});
+
+States.parameters = {
+  variants: [
+    { label: 'Default link', href: 'https://www.webex.com/' },
+    { label: 'Inverted', inverted: true, href: 'https://www.webex.com/' },
+  ],
+};
+
+States.argTypes = { ...argTypes };
+States.argTypes = omit(States.argTypes, ['disabled', 'inverted']);
+
+const LinkHasIcon = Template<LinkProps>(Link).bind({});
+
+LinkHasIcon.args = {
+  href: 'https://www.webex.com/',
+  target: '_blank',
+  tooltipContent: 'opens a new window',
+  tooltipType: 'description',
+};
+
+LinkHasIcon.argTypes = { ...argTypes };
+
+const ButtonCircleLink = Template<LinkProps>(Link).bind({});
+
+ButtonCircleLink.args = {
+  href: 'https://www.webex.com/',
+  tooltipContent: 'opens a new tab',
+  tooltipType: 'description',
+  style: { borderRadius: '100vh' },
+  children: (
+    <div
+      style={{
+        width: '2.5rem',
+        height: '2.5rem',
+        border: 'var(--md-globals-border-clear)',
+        borderWidth: '0.0625rem',
+        cursor: 'pointer',
+        fontFamily: 'var(--md-globals-font-default)',
+        outline: 'none !important',
+        transition: 'background-color 0.2s, color 0.2s, border-color 0.2s',
+        backgroundColor: 'var(--mds-color-theme-button-primary-normal)',
+        color: 'var(--mds-color-theme-text-inverted-normal)',
+        alignItems: 'center',
+        borderRadius: '100vh',
+        display: 'flex',
+        flexShrink: 0,
+        justifyContent: 'space-evenly',
+      }}
+    >
+      <Icon name="chat-group" autoScale={150} />
+    </div>
+  ),
+};
+
+ButtonCircleLink.argTypes = { ...argTypes };
+ButtonCircleLink.argTypes = omit(ButtonCircleLink.argTypes, ['children', 'disabled', 'inverted']);
+
+const ButtonPillLink = Template<LinkProps>(Link).bind({});
+
+ButtonPillLink.args = {
+  href: 'https://www.webex.com/',
+  tooltipContent: 'opens a new tab',
+  tooltipType: 'description',
+  style: { textDecoration: 'none', borderRadius: '100vh' },
+  children: (
+    <div
+      style={{
+        height: '2.5rem',
+        padding: '0 1rem',
+        lineHeight: '2.5rem',
+        border: 'var(--md-globals-border-clear)',
+        borderWidth: '0.0625rem',
+        cursor: 'pointer',
+        fontFamily: 'var(--md-globals-font-default)',
+        outline: 'none !important',
+        transition: 'background-color 0.2s, color 0.2s, border-color 0.2s',
+        backgroundColor: 'var(--mds-color-theme-button-primary-normal)',
+        color: 'var(--mds-color-theme-text-inverted-normal)',
+        alignItems: 'center',
+        borderRadius: '100vh',
+        display: 'flex',
+        flexShrink: 0,
+        justifyContent: 'space-evenly',
+      }}
+    >
+      <Text>Go to Download page</Text>
+      <Icon name="chat-group" autoScale={150} />
+    </div>
+  ),
+};
+
+ButtonPillLink.argTypes = { ...argTypes };
+ButtonPillLink.argTypes = omit(ButtonPillLink.argTypes, ['children', 'disabled', 'inverted']);
+
+export { Example, States, LinkHasIcon, ButtonCircleLink, ButtonPillLink };
---
+++ src/components/Link/Link.unit.test.tsx
@@ -0,0 +1,264 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { DEFAULTS, STYLE } from './Link.constants';
+import { IconNext, LinkNext, TooltipNext } from '@momentum-ui/react-collaboration';
+import { mountAndWait } from '../../../test/utils';
+
+describe('Link', () => {
+  describe('snapshot', () => {
+    let container;
+
+    it('should match snapshot', () => {
+      expect.assertions(1);
+
+      container = mount(<LinkNext>LinkNext</LinkNext>);
+
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should match snapshot when disabled', () => {
+      expect.assertions(1);
+
+      const disabled = !DEFAULTS.DISABLED;
+
+      container = mount(<LinkNext disabled={disabled}>LinkNext</LinkNext>);
+
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should match snapshot when inverted', () => {
+      expect.assertions(1);
+
+      const inverted = !DEFAULTS.INVERTED;
+
+      container = mount(<LinkNext inverted={inverted}>Hyperlink</LinkNext>);
+
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should match snapshot with title', () => {
+      expect.assertions(1);
+
+      const title = 'Example Text';
+
+      const container = mount(<LinkNext title={title} />);
+
+      expect(container).toMatchSnapshot();
+    });
+  });
+
+  describe('attributes', () => {
+    it('should have its main class', () => {
+      expect.assertions(1);
+
+      const element = mount(<LinkNext />)
+        .find(LinkNext)
+        .getDOMNode();
+
+      expect(element.classList.contains(STYLE.wrapper)).toBe(true);
+    });
+
+    it('should have custom class if provided', async () => {
+      const testClass = 'testClass';
+
+      const wrapper = await mount(<LinkNext className={testClass} />);
+
+      const element = wrapper.find('a').getDOMNode();
+
+      expect(element.classList.contains(STYLE.wrapper)).toBe(true);
+      expect(element.classList.contains(testClass)).toBe(true);
+    });
+
+    it('should pass disabled prop', async () => {
+      expect.assertions(1);
+
+      const disabled = !DEFAULTS.DISABLED;
+      const wrapper = await mountAndWait(<LinkNext disabled={disabled} />);
+
+      const element = wrapper.find('a').getDOMNode();
+
+      expect(element.getAttribute('data-disabled')).toBe(`${disabled}`);
+    });
+
+    it('should pass inverted prop', async () => {
+      expect.assertions(1);
+
+      const inverted = !DEFAULTS.INVERTED;
+      const wrapper = await mountAndWait(<LinkNext inverted={inverted} />);
+
+      const element = wrapper.find('a').getDOMNode();
+
+      expect(element.getAttribute('data-inverted')).toBe(`${inverted}`);
+    });
+
+    it('should pass child prop', async () => {
+      expect.assertions(1);
+
+      const child = '1';
+
+      const wrapper = await mountAndWait(<LinkNext>{child}</LinkNext>);
+
+      const element = wrapper.find('a').getDOMNode();
+
+      expect(element.innerHTML).toBe(child);
+    });
+
+    it('should have provided title when title is provided', async () => {
+      expect.assertions(1);
+
+      const title = 'Example Text';
+
+      const wrapper = await mountAndWait(<LinkNext title={title} />);
+
+      const element = wrapper.find('a').getDOMNode();
+
+      expect(element.getAttribute('title')).toBe(title);
+    });
+
+    it('should have Tooltip when tooltipContent is provided', async () => {
+      expect.assertions(1);
+
+      const tooltipContent = 'open a new window';
+
+      const wrapper = await mountAndWait(<LinkNext tooltipContent={tooltipContent} />);
+
+      const element = wrapper.find(TooltipNext);
+
+      expect(element.props()).toEqual({
+        type: undefined,
+        placement: 'bottom',
+        triggerComponent: expect.any(Object),
+        children: tooltipContent,
+      });
+    });
+
+    it('should have Tooltip when tooltipContent and tooltipType is provided', async () => {
+      expect.assertions(1);
+
+      const tooltipContent = 'open a new window';
+
+      const wrapper = await mountAndWait(
+        <LinkNext tooltipContent={tooltipContent} tooltipType={'description'} />
+      );
+
+      const element = wrapper.find(TooltipNext);
+
+      expect(element.props()).toEqual({
+        type: 'description',
+        placement: 'bottom',
+        triggerComponent: expect.any(Object),
+        children: tooltipContent,
+      });
+    });
+
+    it('should have icon when hasExternalLinkIcon is provided', async () => {
+      expect.assertions(1);
+
+      const tooltipContent = 'open a new window';
+
+      const wrapper = await mountAndWait(
+        <LinkNext tooltipContent={tooltipContent} hasExternalLinkIcon={true} />
+      );
+
+      const element = wrapper.find(IconNext);
+
+      expect(element.props()).toEqual({
+        className: STYLE.icon,
+        scale: 16,
+        name: 'pop-out',
+      });
+    });
+
+    it('should have icon when target is _blank', async () => {
+      expect.assertions(1);
+
+      const tooltipContent = 'open a new window';
+
+      const wrapper = await mountAndWait(
+        <LinkNext tooltipContent={tooltipContent} target='_blank' />
+      );
+
+      const element = wrapper.find(IconNext);
+
+      expect(element.props()).toEqual({
+        className: STYLE.icon,
+        scale: 16,
+        name: 'pop-out',
+      });
+    });
+
+    it('should not have icon when target is _blank and hasExternalLinkIcon is false', async () => {
+      expect.assertions(1);
+
+      const tooltipContent = 'open a new window';
+
+      const wrapper = await mountAndWait(
+        <LinkNext tooltipContent={tooltipContent} target='_blank' hasExternalLinkIcon={false} />
+      );
+
+      expect(wrapper.find(IconNext)).toHaveLength(0);
+
+    });
+
+    it('should have icon props when externalLinkIconProps is provided', async () => {
+      expect.assertions(1);
+
+      const tooltipContent = 'open a new window';
+
+      const wrapper = await mountAndWait(
+        <LinkNext tooltipContent={tooltipContent} hasExternalLinkIcon={true} externalLinkIconProps={{ autoScale: true}} />
+      );
+
+      const element = wrapper.find(IconNext);
+
+      expect(element.props()).toEqual({
+        className: STYLE.icon,
+        scale: 16,
+        name: 'pop-out',
+        autoScale: true,
+      });
+    });
+
+    it('should have rel when target is provided', async () => {
+      expect.assertions(1);
+
+      const wrapper = await mountAndWait(<LinkNext target={'_blank'} />);
+
+      const element = wrapper.find('a');
+
+      expect(element.props()).toEqual({
+        rel: 'noopener noreferrer',
+        children: undefined,
+        className: 'md-link-wrapper',
+        'data-disabled': false,
+        'data-inverted': false,
+        elementType: 'a',
+        isDisabled: undefined,
+        isInverted: undefined,
+        target: '_blank',
+        title: undefined,
+        onPress: expect.any(Function)
+      });
+    });
+  });
+
+  describe('actions', () => {
+    it('should handle mouse press events', () => {
+      expect.assertions(1);
+      const mockCallback = jest.fn();
+      const component = mount(<LinkNext onPress={mockCallback} />).find(LinkNext);
+      component.props().onPress({
+        type: 'press',
+        pointerType: 'mouse',
+        altKey: false,
+        shiftKey: false,
+        ctrlKey: false,
+        metaKey: false,
+        target: component.getDOMNode(),
+        continuePropagation: expect.any(Function),
+      });
+
+      expect(mockCallback).toBeCalledTimes(1);
+    });
+  });
+});
