c38549a1786860b3e4beff0ec4bd80c56ad29d6b
Christopher DuBois <chdubois@cisco.com>
2024-08-01 08:04:25 -0700

    fix(listitembase): add height auto option

--- src/components/ListItemBase/ListItemBase.types.ts
+++ src/components/ListItemBase/ListItemBase.types.ts
@@ -1,7 +1,7 @@
 import { CSSProperties, ReactNode } from 'react';
 import { PressEvents } from '@react-types/shared';
 
-export type ListItemBaseSize = 32 | 40 | 50 | 70;
+export type ListItemBaseSize = 32 | 40 | 50 | 70 | 'auto';
 
 export interface ContextMenuState {
   isOpen: boolean;
