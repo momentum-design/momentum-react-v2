c7b52a617c1c214a2a4f050456012aac64f0c452
ndelmar <ndelmar@cisco.com>
2024-09-09 15:38:31 +0100

    feat(menu): remove tabIndex, orientation and isGroupRole from types

--- src/components/Menu/Menu.unit.test.tsx
+++ src/components/Menu/Menu.unit.test.tsx
@@ -168,17 +168,6 @@ describe('<Menu />', () => {
 
       expect(element.getAttribute('data-shape')).toBe(itemShape);
     });
-
-    it('should have provided tabindex when tabIndex is provided', () => {
-      expect.assertions(1);
-
-      const element = mount(<Menu {...defaultProps} itemSize={50} tabIndex={-1} />)
-        .find('div[role="menu"]')
-        .at(0)
-        .getDOMNode();
-
-      expect(element.getAttribute('tabindex')).toBe('-1');
-    });
   });
 
   describe('actions', () => {
