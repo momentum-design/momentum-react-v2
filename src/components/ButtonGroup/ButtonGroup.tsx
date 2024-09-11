20db8990aff39f2101ff1886f67b7667ed6d6d18
Dominic Carrington <docarrin@cisco.com>
2024-09-11 21:30:10 +0100

    fix(ButtonGroup): add forwardRef
    This allows the useKeyboardHook to work in Cantina.

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, forwardRef } from 'react';
 import classNames from 'classnames';
 
 import { DEFAULTS, STYLE, CHILD_OF } from './ButtonGroup.constants';
@@ -7,43 +7,48 @@ import { Props } from './ButtonGroup.types';
 import './ButtonGroup.style.scss';
 
 export interface CompoundProps {
-  CHILD_PROPS: {
+  CHILD_PROPS?: {
     [CHILD_OF.KEY]: typeof CHILD_OF.VALUE;
   };
 }
 
-const ButtonGroup: FC<Props> & CompoundProps = (props: Props) => {
-  const {
-    children,
-    className,
-    id,
-    round,
-    spaced,
-    compressed,
-    separator,
-    style,
-    role,
-    orientation,
-    ...rest
-  } = props;
+const ButtonGroup: FC<Props> & CompoundProps = forwardRef<HTMLDivElement, Props>(
+  (props: Props, ref) => {
+    const {
+      children,
+      className,
+      id,
+      round,
+      spaced,
+      compressed,
+      separator,
+      style,
+      role,
+      orientation,
+      ...rest
+    } = props;
 
-  return (
-    <div
-      className={classNames(STYLE.wrapper, className)}
-      data-round={round || DEFAULTS.ROUND}
-      data-spaced={spaced || DEFAULTS.SPACED}
-      data-compressed={compressed || DEFAULTS.COMPRESSED}
-      data-separator={separator || DEFAULTS.SEPARATOR}
-      data-orientation={orientation || DEFAULTS.ORIENTATION}
-      id={id}
-      style={style}
-      role={role}
-      {...rest}
-    >
-      {children}
-    </div>
-  );
-};
+    return (
+      <div
+        ref={ref}
+        className={classNames(STYLE.wrapper, className)}
+        data-round={round || DEFAULTS.ROUND}
+        data-spaced={spaced || DEFAULTS.SPACED}
+        data-compressed={compressed || DEFAULTS.COMPRESSED}
+        data-separator={separator || DEFAULTS.SEPARATOR}
+        data-orientation={orientation || DEFAULTS.ORIENTATION}
+        id={id}
+        style={style}
+        role={role}
+        {...rest}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+ButtonGroup.displayName = 'ButtonGroup';
 
 export default ButtonGroup;
 
