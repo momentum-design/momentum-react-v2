3825f33b648cc147963a2f18fb7c4ab140392007
Gabriel Lee <chihonglee777@gmail.com>
2024-09-14 12:47:24 +0100

    fix: name

--- src/components/SpaceTreeNode/index.ts
+++ src/components/SpaceTreeNode/index.ts
@@ -2,7 +2,7 @@ import { default as SpaceTreeNode } from './SpaceTreeNode';
 import * as CONSTANTS from './SpaceTreeNode.constants';
 import { Props } from './SpaceTreeNode.types';
 
-export { CONSTANTS as SPACE_LIST_ITEM_CONSTANTS };
+export { CONSTANTS as SPACE_TREE_NODE_CONSTANTS };
 
 export type SpaceTreeNodeProps = Props;
 
