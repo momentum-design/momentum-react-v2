ee13e42e1d244808c02a174857d774dec9f95a15
Shamayl Naaz <shnaaz@cisco.com>
2024-09-18 12:17:09 +0000

    feat(menu): no tick

--- src/components/MenuItem/MenuItem.types.ts
+++ src/components/MenuItem/MenuItem.types.ts
@@ -1,7 +1,7 @@
 import { Key } from 'react';
 import { Node } from '@react-types/shared';
 import { TreeState } from '@react-stately/tree';
-
+export type TickPosition = 'left' | 'right' | 'none';
 export interface Props<T> {
   /**
    * The contents of this menu item
@@ -17,4 +17,15 @@ export interface Props<T> {
    * Handler to be called when this element is selected
    */
   onAction?: (key: Key) => void;
+
+  /**
+ * Position of the tick when selected, none when no tick
+ * @default right
+ */
+  tickPosition?: TickPosition;
+
+  /**
+   * Custom class for overriding this component's CSS when selected.
+   */
+  classNameWhenSelected?: string;
 }
--- src/components/MenuSelectionGroup/MenuSelectionGroup.constants.ts
+++ src/components/MenuSelectionGroup/MenuSelectionGroup.constants.ts
@@ -1,6 +1,9 @@
+import { TickPosition } from './MenuSelectionGroup.types';
 const CLASS_PREFIX = 'md-menu-selection-group';
 
-const DEFAULTS = {};
+const DEFAULTS = {
+  TICK_POSITION: 'right' as TickPosition,
+};
 
 const STYLE = {
   wrapper: `${CLASS_PREFIX}-wrapper`,
