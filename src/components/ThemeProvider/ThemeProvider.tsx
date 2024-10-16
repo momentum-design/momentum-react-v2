34cdfb4851227b95ea7f90c70e335661e433c8ae
Christoph Scheffauer <scheffauerc@gmail.com>
2024-10-16 11:32:25 +0200

    feat: themeprovider migration

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,9 +1,18 @@
 import React, { FC } from 'react';
 
+// TODO: legacy tokens - to be removed:
 import '@momentum-ui/design-tokens/dist/index.css';
+// TODO: we should not use core colors, only theme colors - to be removed:
 import '@momentum-design/tokens/dist/css/core/complete.css';
+
+// import Momentum fonts and typography defaults:
+import '@momentum-design/fonts/dist/css/fonts.css';
+import '@momentum-design/tokens/dist/css/typography/complete.css';
+
+// import Momentum theme tokens:
 import '@momentum-design/tokens/dist/css/theme/webex/dark-stable.css';
 import '@momentum-design/tokens/dist/css/theme/webex/light-stable.css';
+import { ThemeProvider as MdcThemeProvider } from '@momentum-design/components/dist/react';
 
 import {
   DEFAULTS,
@@ -14,21 +23,25 @@ import {
 import { Props } from './ThemeProvider.types';
 import './ThemeProvider.style.scss';
 
+
 /**
  * Provides a collection of CSSVariables based on a ThemeToken to all child elements inside of a rendered `<div />` element.
  */
 const ThemeProvider: FC<Props> = ({ children, id, style, theme }: Props) => {
+  // TODO: get rid of legacy theme
   const themeClass = `${THEME_CLASS_PREFIX}-${theme || DEFAULTS.THEME}`;
   const themeClassStable = `${THEME_CLASS_PREFIX_STABLE}-${theme || DEFAULTS.THEME}`;
 
   return (
-    <div
-      className={`${STYLE.wrapper} ${STYLE.globals} ${themeClass} ${themeClassStable}`}
+    <MdcThemeProvider
+      themeclass={themeClassStable}
+      className={themeClass}
       id={id}
-      style={style}
     >
-      {children}
-    </div>
+      <div className={`${STYLE.wrapper} ${STYLE.globals}`} style={style}>
+        {children}
+      </div>
+    </MdcThemeProvider>
   );
 };
 
