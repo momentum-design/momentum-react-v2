a2ebfe904b359a0226c66b727810ab06f7ba74b3
Dominic Carrington <docarrin@cisco.com>
2024-11-05 12:23:51 +0000

    fix(a11y): remove title being allowed for sr labels

--- src/utils/a11y.ts
+++ src/utils/a11y.ts
@@ -12,13 +12,4 @@ export type AriaLabelRequired =
        * Identifies the element (or elements) that labels the current element.
        */
       'aria-labelledby': AriaAttributes['aria-labelledby'];
-    }
-  | {
-      /**
-       * Defines text representing advisory information related to the element it belongs to.
-       *
-       * @deprecated - Use aria-label or aria-labelledby, using title has a11y concerns
-       * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/title#accessibility_concerns
-       */
-      title: string;
     };
