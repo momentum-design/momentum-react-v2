ef730ced96d26460cb6bd6e4a10e39ed1248470f
Gabriel Lee <chihonglee777@gmail.com>
2024-11-23 17:55:52 +0000

    feat(overlay-alert): require onclose and title or label

--- src/utils/typetest.util.ts
+++ src/utils/typetest.util.ts
@@ -1,6 +1,8 @@
 // https://github.com/type-challenges/type-challenges/blob/main/utils/index.d.ts
 // These types are used for testing to check that 2 types are the same
 export type Expect<T extends true> = T;
+export type ExpectFalse<T extends false> = T;
 export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2
   ? true
   : false;
+export type ExpectExtends<VALUE, EXPECTED> = EXPECTED extends VALUE ? true : false;
