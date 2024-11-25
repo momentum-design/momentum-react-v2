c7909c4f54f4accd2470f52e1d8d6e5a67b1b53f
Dominic Carrington <docarrin@cisco.com>
2024-11-25 17:25:53 +0000

    fix(AriaGroup): fix build

--- src/components/AriaGroup/AriaGroup.types.ts
+++ src/components/AriaGroup/AriaGroup.types.ts
@@ -1,6 +1,6 @@
 import { AriaLabelingProps } from '@react-types/shared';
 import { ReactNode } from 'react';
-import { AriaLabelRequired } from 'src/utils/a11y';
+import { AriaLabelRequired } from '../../utils/a11y';
 
 interface AriaGroupProps extends AriaLabelingProps {
   /**
