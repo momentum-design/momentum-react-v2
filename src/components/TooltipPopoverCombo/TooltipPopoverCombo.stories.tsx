253246bba500bcab08d354b7fcc240b1ee0bcd80
Dominic Carrington <docarrin@cisco.com>
2024-12-02 15:41:02 +0000

    fix(TooltipPopoverCombo): add support for the tooltip to be a description

--- src/components/TooltipPopoverCombo/TooltipPopoverCombo.stories.tsx
+++ src/components/TooltipPopoverCombo/TooltipPopoverCombo.stories.tsx
@@ -13,6 +13,8 @@ import Icon from '../Icon';
 import Menu from '../Menu';
 import { Item } from '@react-stately/collections';
 import { PopoverInstance } from '../Popover/Popover.types';
+import ButtonPill from '../ButtonPill';
+import Flex from '../Flex';
 
 export default {
   title: 'Momentum UI/TooltipPopoverCombo',
@@ -69,4 +71,53 @@ const Example: Story<TooltipPopoverComboProps> = () => {
   );
 };
 
-export { Example };
+const AsDescription: Story<TooltipPopoverComboProps> = () => {
+  const triggerComponent = (
+    <ButtonPill
+      style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: '10rem auto',
+        display: 'flex',
+      }}
+      id="storybook-id-example"
+    >
+      <Flex xgap="0.5rem" alignItems="center">
+        <Icon name="plus" />
+        <Text>Create</Text>
+      </Flex>
+    </ButtonPill>
+  );
+
+  const [popoverInstance, setPopoverInstance] = useState<PopoverInstance>();
+
+  const onItemPress = (key: string) => {
+    switch (key) {
+      default:
+        popoverInstance?.hide();
+    }
+  };
+
+  const popoverContent = (
+    <Menu onAction={onItemPress} style={{ width: '15rem' }}>
+      <Item key="option-1">
+        <Flex xgap="0.5rem" alignItems="center">
+          <Icon name="chat" scale={24} />
+          <Text>Space</Text>
+        </Flex>
+      </Item>
+    </Menu>
+  );
+
+  return (
+    <TooltipPopoverCombo
+      popoverContent={popoverContent}
+      triggerComponent={triggerComponent}
+      tooltipContent={<Text>Press to see options</Text>}
+      otherTooltipProps={{ placement: 'top', type: 'description' }}
+      otherPopoverProps={{ placement: 'top', setInstance: setPopoverInstance }}
+    />
+  );
+};
+
+export { Example, AsDescription };
