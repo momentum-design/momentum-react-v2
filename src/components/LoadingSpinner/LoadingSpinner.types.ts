0ff9d6ddf1acc8b8bc0bcd222a811acaaaebed9c
ndelmar <ndelmar@cisco.com>
2024-12-05 16:05:56 +0000

    feat(loading-spinner): required accessible name or aria-hidden and add role img

--- src/components/LoadingSpinner/LoadingSpinner.types.ts
+++ src/components/LoadingSpinner/LoadingSpinner.types.ts
@@ -1,30 +1,41 @@
 import { CSSProperties, ReactNode } from 'react';
 import { IconScale } from '../Icon';
+import { AriaLabelRequired } from '../../utils/a11y';
+import { AriaLabelingProps } from '@react-types/shared';
 
-export interface Props {
-  /**
-   * Child components of this LoadingSpinner.
-   */
-  children?: ReactNode;
+export type Props = AriaLabelingProps &
+  (
+    | {
+        /**
+         * Whether this loading spinner should be hidden from SR as it's meaning is redundant
+         */
+        'aria-hidden': boolean;
+      }
+    | ({ 'aria-hidden'?: never } & AriaLabelRequired)
+  ) & { // if aria-hidden is not provided, a label is required
+    /**
+     * Child components of this LoadingSpinner.
+     */
+    children?: ReactNode;
 
-  /**
-   * Custom class for overriding this component's CSS.
-   */
-  className?: string;
+    /**
+     * Custom class for overriding this component's CSS.
+     */
+    className?: string;
 
-  /**
-   * Custom id for overriding this component's CSS.
-   */
-  id?: string;
+    /**
+     * Custom id for overriding this component's CSS.
+     */
+    id?: string;
 
-  /**
-   * Custom style for overriding this component's CSS.
-   */
-  style?: CSSProperties;
+    /**
+     * Custom style for overriding this component's CSS.
+     */
+    style?: CSSProperties;
 
-  /**
-   * Size of the loading spinner (same as IconScale).
-   * @default 24
-   */
-  scale?: IconScale;
-}
+    /**
+     * Size of the loading spinner (same as IconScale).
+     * @default 24
+     */
+    scale?: IconScale;
+  };
