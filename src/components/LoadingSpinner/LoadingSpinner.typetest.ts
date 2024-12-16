8658f0cdf0fda34713eff5473161a4e9a4243896
ndelmar <ndelmar@cisco.com>
2024-12-16 13:12:49 +0000

    feat(loading-spinner): fix types

--- src/components/LoadingSpinner/LoadingSpinner.typetest.ts
+++ src/components/LoadingSpinner/LoadingSpinner.typetest.ts
@@ -4,7 +4,9 @@ import { Props } from './LoadingSpinner.types';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 type cases = [
   Expect<ExpectExtends<Props, { 'aria-label': 'Loading' }>>,
+  Expect<ExpectExtends<Props, { 'aria-hidden': false; 'aria-label': 'Loading' }>>,
   Expect<ExpectExtends<Props, { 'aria-labelledby': 'some-id' }>>,
+  Expect<ExpectExtends<Props, { 'aria-hidden': false; 'aria-labelledby': 'some-id' }>>,
   Expect<ExpectExtends<Props, { 'aria-hidden': true }>>,
-  ExpectFalse<ExpectExtends<Props, { 'no-label': '123'; 'no-aria-hidden': true }>>
+  ExpectFalse<ExpectExtends<Props, { 'aria-hidden': false }>>
 ];
