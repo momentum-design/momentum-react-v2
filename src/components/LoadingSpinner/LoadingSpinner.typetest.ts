7ddb130e948077a2b261c16ce1cc5d288a587738
ndelmar <ndelmar@cisco.com>
2024-12-17 11:39:53 +0000

    feat(loading-spinner): fix types and testypes

--- src/components/LoadingSpinner/LoadingSpinner.typetest.ts
+++ src/components/LoadingSpinner/LoadingSpinner.typetest.ts
@@ -4,9 +4,14 @@ import { Props } from './LoadingSpinner.types';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 type cases = [
   Expect<ExpectExtends<Props, { 'aria-label': 'Loading' }>>,
-  Expect<ExpectExtends<Props, { 'aria-hidden': false; 'aria-label': 'Loading' }>>,
+  Expect<ExpectExtends<Props, { 'aria-label': 'Loading'; 'aria-hidden': false }>>,
+  Expect<ExpectExtends<Props, { 'aria-label': 'Loading'; 'aria-hidden': undefined }>>,
+
   Expect<ExpectExtends<Props, { 'aria-labelledby': 'some-id' }>>,
-  Expect<ExpectExtends<Props, { 'aria-hidden': false; 'aria-labelledby': 'some-id' }>>,
+  Expect<ExpectExtends<Props, { 'aria-labelledby': 'some-id'; 'aria-hidden': false }>>,
+  Expect<ExpectExtends<Props, { 'aria-labelledby': 'some-id'; 'aria-hidden': undefined }>>,
+
   Expect<ExpectExtends<Props, { 'aria-hidden': true }>>,
-  ExpectFalse<ExpectExtends<Props, { 'aria-hidden': false }>>
+  ExpectFalse<ExpectExtends<Props, { 'aria-hidden': false }>>,
+  ExpectFalse<ExpectExtends<Props, Record<string, never>>>
 ];
