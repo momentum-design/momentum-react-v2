6f1551608efdf27aeba6de2c8bcf8764b526d9e3
ndelmar <ndelmar@cisco.com>
2025-01-06 18:50:09 +0000

    feat(radio-simple-group): address comments

--- src/components/RadioSimple/RadioSimple.types.ts
+++ src/components/RadioSimple/RadioSimple.types.ts
@@ -1,34 +1,36 @@
 import { CSSProperties, ReactNode } from 'react';
 import { AriaLabelRequired } from '../../utils/a11y';
+import { AriaRadioProps } from '@react-types/radio';
 
-export type RadioSimpleProps = AriaLabelRequired & {
-  /**
-   * Child components of this RadioSimple.
-   */
-  children?: ReactNode;
+export type RadioSimpleProps = AriaLabelRequired &
+  AriaRadioProps & {
+    /**
+     * Child components of this RadioSimple.
+     */
+    children?: ReactNode;
 
-  /**
-   * Custom class for overriding this component's CSS.
-   */
-  className?: string;
+    /**
+     * Custom class for overriding this component's CSS.
+     */
+    className?: string;
 
-  /**
-   * The id for the RadioSimple component.
-   */
-  id?: string;
+    /**
+     * The id for the RadioSimple component.
+     */
+    id?: string;
 
-  /**
-   * Disabled state for the RadioSimple component.
-   */
-  isDisabled?: boolean;
+    /**
+     * Disabled state for the RadioSimple component.
+     */
+    isDisabled?: boolean;
 
-  /**
-   * Custom style for overriding this component's CSS.
-   */
-  style?: CSSProperties;
+    /**
+     * Custom style for overriding this component's CSS.
+     */
+    style?: CSSProperties;
 
-  /**
-   * The value of the RadioSimple component.
-   */
-  value: string;
-};
+    /**
+     * The value of the RadioSimple component.
+     */
+    value: string;
+  };
--- src/components/RadioSimpleGroup/RadioSimpleGroup.types.ts
+++ src/components/RadioSimpleGroup/RadioSimpleGroup.types.ts
@@ -1,36 +1,33 @@
 import { CSSProperties, ReactNode } from 'react';
 import { AriaRadioGroupProps } from '@react-types/radio';
 import { AriaLabelingProps } from '@react-types/shared';
-import { AriaLabelRequired } from '../../utils/a11y';
+import { RequireOneOf } from '../../utils/types';
 
-export type RadioSimpleGroupProps = AriaRadioGroupProps &
-  AriaLabelingProps &
-  (
-    | {
-        /**
-         * The RadioSimpleGroup's visible label (if any).
-         */
-        label: ReactNode;
-      }
-    | ({ label?: never } & AriaLabelRequired)
-  ) & { // if a label is not provided, an aria-label(ledby) is required
-    /**
-     * Custom class for overriding this component's CSS.
-     */
-    className?: string;
+interface Props extends AriaRadioGroupProps, AriaLabelingProps {
+  /**
+   * The RadioSimpleGroup's visible label (if any).
+   */
+  label?: ReactNode;
+  /**
+   * Custom class for overriding this component's CSS.
+   */
+  className?: string;
 
-    /**
-     * A wrapper that contains the RadioSimples inside this RadioSimpleGroup
-     */
-    children: ReactNode;
+  /**
+   * A wrapper that contains the RadioSimples inside this RadioSimpleGroup
+   */
+  children: ReactNode;
 
-    /**
-     *  The RadioSimpleGroup description element, if any.
-     */
-    description?: ReactNode;
+  /**
+   *  The RadioSimpleGroup description element, if any.
+   */
+  description?: ReactNode;
 
-    /**
-     * Custom style for overriding this component's CSS.
-     */
-    style?: CSSProperties;
-  };
+  /**
+   * Custom style for overriding this component's CSS.
+   */
+  style?: CSSProperties;
+}
+
+export type RadioSimpleGroupProps = Props &
+  RequireOneOf<Props, ['label', 'aria-label', 'aria-labelledby']>;
