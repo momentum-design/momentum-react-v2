2a46f90d038d5f842abedd2dc27a048b7b987e7b
Gabriel Lee <chihonglee777@gmail.com>
2025-03-15 21:33:37 +0000

    feat(coachmark): create coachmark web component wrapper

--- src/components/Coachmark/index.ts
+++ src/components/Coachmark/index.ts
@@ -1,8 +1,5 @@
 import { default as Coachmark } from './Coachmark';
-import * as CONSTANTS from './Coachmark.constants';
 import { Props } from './Coachmark.types';
-
-export { CONSTANTS as COACHMARK_CONSTANTS };
 
 export type CoachmarkProps = Props;
 
