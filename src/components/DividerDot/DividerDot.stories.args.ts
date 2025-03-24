54283c666efb92e61d978129d7776a362fcb600c
Christoph Scheffauer <scheffauerc@gmail.com>
2025-03-24 17:47:51 +0100

    fix: migrate dividerdot by using bullet component

--- src/components/DividerDot/DividerDot.stories.args.ts
+++ src/components/DividerDot/DividerDot.stories.args.ts
@@ -1,5 +1,19 @@
 import { commonStyles } from '../../storybook/helper.stories.argtypes';
+import { DEFAULTS } from './DividerDot.constants';
 
 export default {
   ...commonStyles,
+  size: {
+    description: 'To set the size of DividerDot',
+    options: ['small', 'medium', 'large'],
+    control: { type: 'select' },
+    table: {
+      type: {
+        summary: 'string',
+      },
+      defaultValue: {
+        summary: DEFAULTS.SIZE,
+      },
+    },
+  },
 };
