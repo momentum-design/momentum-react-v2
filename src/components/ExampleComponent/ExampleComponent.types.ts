5c882ae6e145173b5c6829f82afcbf3d663a8227
Christoph Scheffauer <scheffauerc@gmail.com>
2025-03-24 14:58:06 +0100

    fix: remove unused components

--- src/components/ExampleComponent/ExampleComponent.types.ts
+++
@@ -1,3 +0,0 @@
-export interface Props {
-  param: string;
-}
--- src/components/ExampleComponent/index.ts
+++
@@ -1,9 +0,0 @@
-import { default as ExampleComponent } from './ExampleComponent';
-import * as CONSTANTS from './ExampleComponent.constants';
-import { Props } from './ExampleComponent.types';
-
-export { CONSTANTS as EXAMPLE_COMPONENT_CONSTANTS };
-
-export type ExampleComponentProps = Props;
-
-export default ExampleComponent;
