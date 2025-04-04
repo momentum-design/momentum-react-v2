0706f8109358f0132ba123cbd37efd586af09e96
Christoph Scheffauer <scheffauerc@gmail.com>
2025-04-04 17:36:22 +0200

    fix: cleanup legacy components

--- src/legacy/Label/examples/Default.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Label } from '@momentum-ui/react-collaboration';
-
-export default function LabelDefault() {
-  return <Label htmlFor="label1" label="Default Label" />;
-}
