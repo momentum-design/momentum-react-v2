e8b7ab43dcd9de18cc121636b5d65338ae5b70ba
Christoph Scheffauer <scheffauerc@gmail.com>
2025-04-04 17:36:22 +0200

    fix: cleanup legacy components

--- src/legacy/EditableTextfield/examples/Default.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { EditableTextfield } from '@momentum-ui/react-collaboration';
-export default class EditableTextFieldDefault extends React.Component {
-  render() {
-    return (
-      <EditableTextfield handleDoneEditing={(e, data) => alert(data)} inputText="Hello World" />
-    );
-  }
-}
