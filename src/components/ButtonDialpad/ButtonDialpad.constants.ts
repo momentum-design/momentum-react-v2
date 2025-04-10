01d07f377ed2d2d5a34a4a0d12c04be0c5286e75
Christoph Scheffauer <scheffauerc@gmail.com>
2025-04-10 15:02:38 +0200

    fix: migrate buttondialpad

--- src/components/ButtonDialpad/ButtonDialpad.constants.ts
+++ src/components/ButtonDialpad/ButtonDialpad.constants.ts
@@ -14,6 +14,7 @@ const STYLE = {
   primaryText: `${CLASS_PREFIX}-primary-text`,
   secondaryText: `${CLASS_PREFIX}-secondary-text`,
   wrapper: `${CLASS_PREFIX}-wrapper`,
+  innerWrapper: `${CLASS_PREFIX}-inner-wrapper`,
 };
 
 export { CLASS_PREFIX, DEFAULTS, SIZES, STYLE };
