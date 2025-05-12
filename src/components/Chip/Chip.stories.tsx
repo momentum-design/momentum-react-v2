adc1625252f54087392fcdc7443e56049f8c8556
Christoph Scheffauer <scheffauerc@gmail.com>
2025-05-12 16:19:50 +0200

    fix: chip migration

--- src/components/Chip/Chip.stories.tsx
+++ src/components/Chip/Chip.stories.tsx
@@ -1,13 +1,10 @@
-import { MultiTemplate, Template } from '../../storybook/helper.stories.templates';
+import { Template } from '../../storybook/helper.stories.templates';
 import { DocumentationPage } from '../../storybook/helper.stories.docs';
 import StyleDocs from '../../storybook/docs.stories.style.mdx';
 
-import Chip, { ChipProps, CHIP_CONSTANTS } from './';
+import Chip, { ChipProps } from './';
 import argTypes from './Chip.stories.args';
 import Documentation from './Chip.stories.docs.mdx';
-import Icon from '../Icon';
-import React from 'react';
-import Avatar from '../Avatar';
 
 export default {
   title: 'Momentum UI/Chip',
@@ -25,102 +22,8 @@ const Example = Template<ChipProps>(Chip).bind({});
 Example.argTypes = { ...argTypes };
 
 Example.args = {
-  children: 'Example',
-  text: 'Hello World',
+  label: 'Example',
+  color: 'default',
 };
 
-const exampleIcon = <Icon name="placeholder" scale={16} />;
-const exampleRightIcon = <Icon name="cancel" scale={16} />;
-const exampleAvatar = <Avatar initials="AA" size={24} />;
-const Common = MultiTemplate<ChipProps>(Chip).bind({});
-
-Common.argTypes = { ...argTypes };
-delete Common.argTypes.children;
-
-Common.args = {
-  children: 'Example',
-  style: { margin: '1rem' },
-};
-
-Common.parameters = {
-  variants: [
-    { children: 'Chip with icons', leftIcon: exampleIcon, rightIcon: exampleRightIcon },
-    {
-      children: 'Chip with avatar',
-      avatar: exampleAvatar,
-      leftIcon: exampleIcon,
-      rightIcon: exampleRightIcon,
-    },
-    {
-      children: 'With:',
-      size: 28,
-      search: true,
-    },
-    {
-      children: 'With:',
-      size: 24,
-      search: true,
-    },
-    { children: 'Disabled chip', disabled: true },
-    {
-      children: 'Outline ghost chip',
-      leftIcon: exampleIcon,
-      rightIcon: exampleRightIcon,
-      outline: true,
-      ghost: true,
-    },
-    {
-      children: 'Lonnnnnnnnnnnnnnng outline ghost chip',
-      leftIcon: exampleIcon,
-      rightIcon: exampleRightIcon,
-      outline: true,
-      ghost: true,
-    },
-    {
-      children: 'Disabled outline ghost chip',
-      leftIcon: exampleIcon,
-      rightIcon: exampleRightIcon,
-      outline: true,
-      ghost: true,
-      disabled: true,
-    },
-    {
-      children: 'Error chip',
-      leftIcon: exampleIcon,
-      rightIcon: exampleRightIcon,
-      error: true,
-    },
-    {
-      children: 'Error outline ghost chip',
-      leftIcon: exampleIcon,
-      rightIcon: exampleRightIcon,
-      outline: true,
-      ghost: true,
-      error: true,
-    },
-    {
-      children: 'Error outline ghost chip with avatar',
-      avatar: exampleAvatar,
-      outline: true,
-      ghost: true,
-      error: true,
-    },
-  ],
-};
-
-const Colors = MultiTemplate<ChipProps>(Chip).bind({});
-
-Colors.args = {
-  style: { margin: '1rem' },
-};
-
-Colors.parameters = {
-  variants: Object.values(CHIP_CONSTANTS.MULTILINE_COLORS).map((color) => ({
-    children: `chipColor = ${color}`,
-    chipColor: color,
-    size: 16,
-    multiline: true,
-  })),
-};
-
-export { Example, Common, Colors };
+export { Example };
--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,67 +1,14 @@
 import React, { FC } from 'react';
-import classnames from 'classnames';
 
-import { STYLE, DEFAULTS } from './Chip.constants';
 import { Props } from './Chip.types';
-import './Chip.style.scss';
 
-import ButtonPill from '../ButtonPill';
-import Text from '../Text';
+import { Chip as MdcChip } from '@momentum-design/components/dist/react';
 
 /**
  * The Chip component.
  */
 const Chip: FC<Props> = (props: Props) => {
-  const {
-    text,
-    avatar = DEFAULTS.AVATAR,
-    leftIcon,
-    rightIcon,
-    className,
-    id,
-    style,
-    children,
-    size = DEFAULTS.SIZE,
-    chipColor = DEFAULTS.COLOR,
-    error = DEFAULTS.ERROR,
-    multiline = DEFAULTS.MULTILINE,
-    search = DEFAULTS.SEARCH,
-  } = props;
-
-  const leftContent = avatar ? avatar : leftIcon;
-  const changedText = text ? text : children;
-  const secondaryStyle = multiline || (size === 24 && search);
-
-  return (
-    <ButtonPill
-      size={size}
-      data-size={size}
-      className={classnames(className, STYLE.wrapper)}
-      id={id}
-      style={style}
-      data-error={error}
-      data-modifier-color={chipColor}
-      data-multiline={multiline}
-      data-search={search}
-      {...props}
-    >
-      {leftContent && (
-        <div data-avatar={!!avatar} className={STYLE.leftSection}>
-          {leftContent}
-        </div>
-      )}
-      <Text
-        type={secondaryStyle ? 'subheader-secondary' : 'body-primary'}
-        tagName={secondaryStyle ? 'h3' : 'p'}
-        data-search={search}
-        data-multiline={multiline}
-        className={STYLE.centerSection}
-      >
-        {changedText}
-      </Text>
-      {rightIcon && <div className={STYLE.rightSection}>{rightIcon}</div>}
-    </ButtonPill>
-  );
+  return <MdcChip {...props} />;
 };
 
 export default Chip;
