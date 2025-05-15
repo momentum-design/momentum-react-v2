a46c857ca2ca68a85149159c04c37cf8380cb85b
Christoph Scheffauer <scheffauerc@gmail.com>
2025-05-15 16:01:03 +0200

    fix: chip stoppropagation

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -8,7 +8,34 @@ import { Chip as MdcChip } from '@momentum-design/components/dist/react';
  * The Chip component.
  */
 const Chip: FC<Props> = (props: Props) => {
-  return <MdcChip {...props} />;
+  const { onClick, stopPropagation = false, ...rest } = props;
+
+  // this is a workaround for now for cases where usePress from react-aria is used on a parent (i.e. ListItemBase)
+  // of the chip. We want to avoid the event bubbling up to the parent in those cases
+  // this can be removed once ListItemBase is refactored to use the new momentum-design listitem component
+  const preventBubble = (event: any) => {
+    // Prevent the event from bubbling up to the parent element
+    if (stopPropagation) {
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  };
+
+  const handleClick = (event: MouseEvent) => {
+    preventBubble(event);
+    if (onClick) {
+      onClick(event);
+    }
+  };
+
+  return (
+    <MdcChip
+      onClick={handleClick}
+      onPointerUp={preventBubble}
+      onPointerDown={preventBubble}
+      {...rest}
+    />
+  );
 };
 
 export default Chip;
--- src/components/Chip/Chip.types.ts
+++ src/components/Chip/Chip.types.ts
@@ -3,4 +3,6 @@ import type { Chip } from '@momentum-design/components/dist/react';
 // props type of chip
 type MdcChipProps = React.ComponentProps<typeof Chip>;
 
-export type Props = MdcChipProps;
+export type Props = MdcChipProps & {
+  stopPropagation?: boolean;
+};
