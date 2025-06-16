cfa88288aee085881f0395b60584203ef8b973cb
Christoph Scheffauer <scheffauerc@gmail.com>
2025-06-16 17:02:32 +0200

    fix: popover & tooltip escape issue in dialog

--- src/components/Dialog/Dialog.stories.tsx
+++ src/components/Dialog/Dialog.stories.tsx
@@ -8,6 +8,9 @@ import ButtonPill from '../ButtonPill';
 import Dialog, { DialogProps } from './';
 import argTypes from './Dialog.stories.args';
 import Documentation from './Dialog.stories.docs.mdx';
+import Icon from '../Icon';
+import Tooltip from '../Tooltip';
+import ButtonCircle from '../ButtonCircle';
 
 export default {
   title: 'Momentum UI/Dialog',
@@ -93,4 +96,83 @@ Example.parameters = {
 
 Example.args = { ...coreArgs };
 
-export { Example };
+const ContainsPopovers: Story<DialogProps> = (args: DialogProps, { parameters }) => {
+  const { hasActions } = parameters;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = () => {
+    setIsOpen(true);
+  };
+
+  const close = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div
+      style={{
+        alignItems: 'center',
+        backgroundColor: 'var(--mds-color-theme-background-solid-primary-normal)',
+        border:
+          '1px var(--md-globals-border-style-solid) var(--mds-color-theme-outline-secondary-normal)',
+        display: 'flex',
+        height: '80%',
+        paddingLeft: '4rem',
+        position: 'relative',
+        width: '80%',
+      }}
+    >
+      <ButtonPill onClick={open}>Open</ButtonPill>
+      {isOpen && (
+        <Dialog
+          {...args}
+          footerButtonPrimary={
+            hasActions ? (
+              <ButtonPill onClick={close} size={32}>
+                Primary
+              </ButtonPill>
+            ) : undefined
+          }
+          footerButtonSecondary={
+            hasActions ? (
+              <ButtonPill variant="secondary" onClick={close} size={32}>
+                Secondary
+              </ButtonPill>
+            ) : undefined
+          }
+          onClose={close}
+          {...args}
+        >
+          <div
+            style={{
+              marginTop: '2rem',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            Focus on this button. Closing the tooltip using Esc will not close the OverlayAlert.
+            <Tooltip
+              placement="top"
+              triggerComponent={
+                <ButtonCircle>
+                  <Icon name="accessibility" />
+                </ButtonCircle>
+              }
+              type="label"
+            >
+              Tooltip content
+            </Tooltip>
+          </div>
+        </Dialog>
+      )}
+    </div>
+  );
+};
+
+ContainsPopovers.argTypes = { ...argTypes };
+
+ContainsPopovers.parameters = {
+  hasActions: true,
+};
+export { Example, ContainsPopovers };
