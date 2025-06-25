627cd4671bc4ac3ec39ac93a37820fbbe242114f
Christoph Scheffauer <scheffauerc@gmail.com>
2025-06-25 17:53:22 +0200

    fix: changing text types and updating unit tests

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,4 +1,5 @@
-import type { Button, Dialog } from '@momentum-design/components/dist/react';
+import type { Button, Dialog, Text } from '@momentum-design/components/dist/react';
 
 export type MdcButtonProps = React.ComponentProps<typeof Button>;
 export type MdcDialogProps = React.ComponentProps<typeof Dialog>;
+export type MdcTextProps = React.ComponentProps<typeof Text>;
