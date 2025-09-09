dab99ef45aeb0bccb3f876b30b495cc9cbaa979f
Christoph Scheffauer <scheffauerc@gmail.com>
2025-09-09 16:09:34 +0200

    fix: remove legacy component folders

--- src/legacy/index.js
+++ src/legacy/index.js
@@ -2,14 +2,10 @@
  * Entry file for momentum-ui-react module.
  */
 export { default as AlertBanner } from './AlertBanner';
-export { default as AlertContainer } from './AlertContainer';
-export { default as Avatar } from './Avatar';
 export { default as Badge } from './Badge';
 export { default as Button } from './Button';
 export { default as ButtonGroup } from './ButtonGroup';
-export { default as Chip } from './Chip';
 export { default as ContentItem } from './ContentItem';
-export { default as Form } from './Form';
 export { default as Icon } from './Icon';
 export { default as Input } from './Input';
 export { default as Lightbox } from './Lightbox';
@@ -19,21 +15,12 @@ export { default as ListItem } from './ListItem';
 export { default as ListItemHeader } from './ListItemHeader';
 export { default as ListItemSection } from './ListItemSection';
 export { default as Loading } from './Loading';
-export { default as Menu } from './Menu';
-export { default as MenuContent } from './MenuContent';
-export { default as MenuOverlay } from './MenuOverlay';
-export { default as MenuItem } from './MenuItem';
 export { default as Modal } from './Modal';
 export { default as ModalBody } from './ModalBody';
 export { default as ModalFooter } from './ModalFooter';
 export { default as ModalHeader } from './ModalHeader';
 export { default as Popover } from './Popover';
-export { default as ProgressBar } from './ProgressBar';
-export { default as Select } from './Select';
-export { default as SelectOption } from './SelectOption';
 export { default as SpaceListItem } from './SpaceListItem';
-export { default as Spinner } from './Spinner';
-export { default as SubMenu } from './SubMenu';
 export { default as Tooltip } from './Tooltip';
 
 import * as utils from './utils';
